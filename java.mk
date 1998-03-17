41a87de8fb90dbb8e178676f989fd7cf1da366c5
tmitchel <tmitchel@f68c23cc-7f87-4fbc-acfa-d9760b90acc1>
1998-03-17 18:10:21 +0000

    updated copyright notice
    git-svn-id: svn+ssh://src.dsl.bbn.com/svn/openmap/trunk@3 f68c23cc-7f87-4fbc-acfa-d9760b90acc1

--- java.mk
+++ java.mk
@@ -1,23 +1,28 @@
 # **********************************************************************
 # 
-#  BBN Corporation
-#  10 Moulton St.
-#  Cambridge, MA 02138
-#  (617) 873-2000
-# 
-#  Copyright (C) 1997
-#  This software is subject to copyright protection under the laws of 
-#  the United States and other countries.
-# 
+#    Use, duplication, or disclosure by the Government is subject to
+# 	     restricted rights as set forth in the DFARS.
+#  
+# 			   BBN Technologies
+# 			    A Division of
+# 			   BBN Corporation
+# 			  10 Moulton Street
+# 			 Cambridge, MA 02138
+# 			    (617) 873-3000
+#  
+# 	  Copyright 1998 by BBN Technologies, A Division of
+# 		BBN Corporation, all rights reserved.
+#  
 # **********************************************************************
 # 
 # $Source: /cvs/distapps/openmap/Attic/java.mk,v $
 # $RCSfile: java.mk,v $
-# $Revision: 1.1 $
-# $Date: 1998/03/16 22:41:45 $
+# $Revision: 1.2 $
+# $Date: 1998/03/17 18:10:21 $
 # $Author: tmitchel $
 # 
 # **********************************************************************
+
 
 # add some new suffixes for java related files
 .SUFFIXES: .java .class
