8a9f6630f19896a9875f8a5da2f180bfbe6aecc2
rmcneil <rmcneil@f68c23cc-7f87-4fbc-acfa-d9760b90acc1>
1998-11-23 04:20:20 +0000

    Fix for final release name.
    git-svn-id: svn+ssh://src.dsl.bbn.com/svn/openmap/trunk@118 f68c23cc-7f87-4fbc-acfa-d9760b90acc1

--- bin/openmap.bat
+++ bin/openmap.bat
@@ -2,7 +2,7 @@ rem This file run the OpenMap application
 
 rem Set up some environment variables
 set SWINGHOME=D:\jdk1.1.6\swing-1.0.2
-set OM_HOME=E:\openmap-3.0beta2
+set OM_HOME=E:\openmap-3.0
 set CLASSPATH=%CLASSPATH%;%SWINGHOME%\swing.jar;%OM_HOME%;%OM_HOME%\share
 set JAVABIN=java
 
