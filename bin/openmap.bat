87090f944b32e61bab929e51900dbb16e932e2e0
adoyle <adoyle@f68c23cc-7f87-4fbc-acfa-d9760b90acc1>
1999-01-26 15:02:46 +0000

    Fixed for Java 1.2 and OpenMap 3.1.2
    git-svn-id: svn+ssh://src.dsl.bbn.com/svn/openmap/trunk@176 f68c23cc-7f87-4fbc-acfa-d9760b90acc1

--- bin/openmap.bat
+++ bin/openmap.bat
@@ -1,9 +1,14 @@
-rem This file run the OpenMap application
+rem This file works with OpenMap 3.1.2 or greater using Java 1.2 (Java 2)
+rem   This file run the OpenMap application
 
 rem Set up some environment variables
-set SWINGHOME=D:\jdk1.1.6\swing-1.0.2
-set OM_HOME=E:\openmap-3.0
-set CLASSPATH=%CLASSPATH%;%SWINGHOME%\swing.jar;%OM_HOME%;%OM_HOME%\share
+
+rem This is where the OpenMap top-level directories are.
+rem CHANGE THIS TO MATCH YOUR CONFIGURATION
+set OM_HOME=C:\home\adoyle\openmap
+
+rem This assumes your CLASSPATH is already set
+set CLASSPATH=%CLASSPATH%;%OM_HOME%;%OM_HOME%\share
 set JAVABIN=java
 
 rem OK, now run it
