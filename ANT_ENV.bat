b6344ca963768410990a033cbe09f31989adbe54
pmanghwa <pmanghwa@f68c23cc-7f87-4fbc-acfa-d9760b90acc1>
2001-02-27 00:52:50 +0000

    Developers can use this file to initialize ANT environment, if they haven't done so before executing ant. Ex: ANT_ENV ant
    git-svn-id: svn+ssh://src.dsl.bbn.com/svn/openmap/trunk@312 f68c23cc-7f87-4fbc-acfa-d9760b90acc1

---
+++ ANT_ENV.bat
@@ -0,0 +1,10 @@
+rem These environment variables are required so that Ant works correctly in Dos.
+rem If you have set them, then there is no need to run this file.
+rem Otherwise update the paths to reflect your environment.
+
+set ANT_HOME=K:\Ant
+set JAVA_HOME=F:\jdk1.2.2
+set PATH=%ANT_HOME%\bin;%PATH%
+
+rem This is optional if you already have java.exe in your path
+rem set PATH=%JAVA_HOME%\bin;%PATH%
