a6ca2cb9270566acb64f048c66c644bbf71173c0
bmackiew <bmackiew@f68c23cc-7f87-4fbc-acfa-d9760b90acc1>
2001-11-16 03:19:21 +0000

    Updated the directions for using Ant to recompile OpenMap under Windows.
    git-svn-id: svn+ssh://src.dsl.bbn.com/svn/openmap/trunk@385 f68c23cc-7f87-4fbc-acfa-d9760b90acc1

--- ANT_ENV.bat
+++ ANT_ENV.bat
@@ -2,9 +2,10 @@ rem These environment variables are required so that Ant works correctly in Dos.
 rem If you have set them, then there is no need to run this file.
 rem Otherwise update the paths to reflect your environment.
 
-set ANT_HOME=K:\Ant
-set JAVA_HOME=F:\jdk1.2.2
+set ANT_HOME=C:\jakarta-ant-1.4.1
+set JAVA_HOME=C:\jdk1.3.1_01
 set PATH=%ANT_HOME%\bin;%PATH%
+set CLASSPATH=%CLASSPATH%;%ANT_HOME%\lib\ant.jar;%ANT_HOME%\lib\crimson.jar;%ANT_HOME%\lib\jaxp.jar
 
 rem This is optional if you already have java.exe in your path
 rem set PATH=%JAVA_HOME%\bin;%PATH%
