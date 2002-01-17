a99d8c084f4f01602d6ff8113bf9dcfb70081c9d
bmackiew <bmackiew@f68c23cc-7f87-4fbc-acfa-d9760b90acc1>
2002-01-17 19:34:55 +0000

    Made some changes per Ken Anderson's recommendations.  Set the default Java location to assume that java.exe is in the developer's path, and set the OM_HOME to be ".." - that should work just fine unless the openmap.bat file is moved from the bin/ directory.
    git-svn-id: svn+ssh://src.dsl.bbn.com/svn/openmap/trunk@395 f68c23cc-7f87-4fbc-acfa-d9760b90acc1

--- bin/openmap.bat
+++ bin/openmap.bat
@@ -4,12 +4,13 @@ rem This will launch the OpenMap application
 rem YOU MUST EDIT THE SETTINGS IN THIS FILE TO MATCH YOUR CONFIGURATION
 
 rem Java Virtual Machine
-rem CHANGE THIS TO MATCH YOUR CONFIGURATION
-set JAVABIN=C:\jdk1.2\bin\java.exe
+rem CHANGE THIS TO POINT TO YOUR JAVA INSTALLATION IF JAVA ISN"T IN YOUR PATH
+set JAVABIN=java.exe
 
 rem OpenMap top-level directory
-rem CHANGE THIS TO MATCH YOUR CONFIGURATION
-set OM_HOME=C:\openmap
+rem THIS POINTS TO THE DIRECTORY ABOVE "BIN". CHANGE THIS IF YOU MOVE THE
+rem BATCH FILE.
+set OM_HOME=..
 
 rem CLASSPATH points to toplevel OpenMap directory and share subdirectory
 set CLASSPATH=%OM_HOME%;%OM_HOME%\share;%OM_HOME%\lib\openmap.jar
