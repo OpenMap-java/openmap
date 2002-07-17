d5e96a6eaa33448a1fb1c503ea3bb2b424f95995
dietrick <dietrick@f68c23cc-7f87-4fbc-acfa-d9760b90acc1>
2002-07-17 17:30:32 +0000

    Updated bin directory, with customized ant scripts, ant-derived openmap lauch script.
    git-svn-id: svn+ssh://src.dsl.bbn.com/svn/openmap/trunk@448 f68c23cc-7f87-4fbc-acfa-d9760b90acc1

--- bin/openmap.bat
+++ bin/openmap.bat
@@ -10,10 +10,10 @@ set JAVABIN=java.exe
 rem OpenMap top-level directory
 rem THIS POINTS TO THE DIRECTORY ABOVE "BIN". CHANGE THIS IF YOU MOVE THE
 rem BATCH FILE.
-set OM_HOME=..
+set OPENMAP_HOME=..
 
 rem CLASSPATH points to toplevel OpenMap directory and share subdirectory
-set CLASSPATH=%OM_HOME%;%OM_HOME%\share;%OM_HOME%\lib\openmap.jar
+set CLASSPATH=%OPENMAP_HOME%;%OPENMAP_HOME%\share;%OPENMAP_HOME%\lib\openmap.jar
 
 rem OK, now run OpenMap
-%JAVABIN% -mx64m -Dopenmap.configDir=%OM_HOME%\share com.bbn.openmap.app.OpenMap
+%JAVABIN% -mx64m -Dopenmap.configDir=%OPENMAP_HOME%\share com.bbn.openmap.app.OpenMap
