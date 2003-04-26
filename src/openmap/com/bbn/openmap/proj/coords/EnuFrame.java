3eb75b32eaa4c5f7f5b52d173bbce3d4fe12bbaa
dietrick <dietrick@f68c23cc-7f87-4fbc-acfa-d9760b90acc1>
2003-04-26 00:50:05 +0000

    Adding classes from Robert Hayes
    git-svn-id: svn+ssh://src.dsl.bbn.com/svn/openmap/trunk@699 f68c23cc-7f87-4fbc-acfa-d9760b90acc1

---
+++ src/openmap/com/bbn/openmap/proj/coords/EnuFrame.java
@@ -0,0 +1,292 @@
+package  com.bbn.openmap.proj.coords;
+
+import com.bbn.openmap.LatLonPoint;
+import com.bbn.openmap.proj.ProjMath;
+
+/**
+ * Encapsulates the ENU (East-North-Up) coordinate system.
+ * <p>
+ * I like the idea of hiding the detail of the transformation from the user.
+ * The user needs no detailed knowledge of mathematics and
+ * if we change the algorithm, nobody will be affected.
+ * <p>
+ * Author: Robert Hayes
+ */
+public class EnuFrame {
+
+    /**
+     * The x component of the coordinate (EAST).
+     */
+    protected float x;
+    /**
+     * The y component of the coordinate (NORTH).
+     */
+    protected float y;
+    /**
+     * The z component of the coordinate (UP).
+     */
+    protected float z;
+    
+    /**
+     * Construct a default EnuFrame.
+     * x,y,z are all set to zero.
+     */
+    public EnuFrame () {
+    	x = 0.0f;
+    	y = 0.0f;
+    	z = 0.0f;
+
+    }
+
+    /**
+     * Construct a EnuFrame from a ECEF vector and a LatLonPoint.
+     *
+     * @param llpt LatLonPoint.
+     * @param double[] ecefv representing an ecef vector.
+     */
+    public EnuFrame(double[] ecefv, LatLonPoint llpt) {
+    	double lat_ = (double)llpt.getLatitude();
+    	double lon_ = (double)llpt.getLongitude();
+    	
+    	double latitude = ProjMath.degToRad(lat_);
+    	double longitude = ProjMath.degToRad(lon_);
+    	
+    	double ecefVector[] = new double[3];
+    	double enuVector[] = new double[3];
+    	
+    	ecefVector[0] = ecefv[0];
+    	ecefVector[1] = ecefv[1];
+    	ecefVector[2] = ecefv[2];
+    	
+    	ecef2enu(ecefVector, latitude, longitude, enuVector);
+    	
+    	this.x = (float)enuVector[0];
+    	this.y = (float)enuVector[1];
+    	this.z = (float)enuVector[2];
+       
+    }
+    
+    /**
+     * Convert to geocentric frame using a LatLonPoint.
+     *
+     * @param llpt.
+     * @return a double of ecef values
+     */
+    public double[] toGeocentricFrame(LatLonPoint llpt) {
+        double lat_ = (double)llpt.getLatitude();
+    	double lon_ = (double)llpt.getLongitude();
+    	
+    	double latitude = ProjMath.degToRad(lat_);
+    	double longitude = ProjMath.degToRad(lon_);
+    	
+    	double enuVector[] = new double[3];	
+    	double ecefVector[] = new double[3];
+    	
+    	enuVector[0] = this.x;
+    	enuVector[1] = this.y;
+    	enuVector[2] = this.z;
+    	
+    	enu2ecef(ecefVector, latitude, longitude, enuVector);
+    	
+    	return ecefVector;
+    }
+
+    /**
+     * Internal conversion routine.
+     *
+     * @param ecef vector
+     * @param latitude in radians.
+     * @param longitude in radians
+     * @param enu vector
+     */
+    public void ecef2enu(double ecefVector[], double latitude, double longitude, double enuVector[]) {
+
+        double temp[][] = new double[3][3];
+        double slat = Math.sin(latitude);
+        double clat = Math.cos(latitude);
+        double slon = Math.sin(longitude);
+        double clon = Math.cos(longitude);
+
+        temp[0][0] = -slon;
+        temp[0][1] = clon;
+        temp[0][2] = 0.0;
+        
+        temp[1][0] = -clon * slat;
+        temp[1][1] = -slon * slat;
+        temp[1][2] = clat;
+        
+        temp[2][0] = clon * clat;
+        temp[2][1] = slon * clat;
+        temp[2][2] = slat;
+        
+        for(int j=0; j<3; ++j) {
+	    enuVector[j] = 0.0;
+	    for(int i=0; i<3; i++ ){
+		enuVector[j] += temp[j][i] * ecefVector[i]; 
+            }
+	}	    
+    }
+
+    /**
+     * Internal conversion routine.
+     *
+     * @param ecef vector
+     * @param latitude in radians.
+     * @param longitude in radians
+     * @param enu vector
+     */
+    protected void enu2ecef(double ecefVector[], double latitude, double longitude, double enuVector[]) {
+
+        double temp[][] = new double[3][3];
+        double clat = Math.cos(latitude);
+        double slat = Math.sin(latitude);
+        double clon = Math.cos(longitude);
+        double slon = Math.sin(longitude);
+
+        temp[0][0] = -slon;
+        temp[0][1] = -clon * slat;
+        temp[0][2] = clon * clat;
+        
+        temp[1][0] = clon;
+        temp[1][1] = -slon * slat;
+        temp[1][2] = slon * clat;
+        
+        temp[2][0] = 0.0;
+        temp[2][1] = clat;
+        temp[2][2] = slat;
+        
+        for(int j=0; j<3; ++j) {
+	    ecefVector[j] = 0.0;
+	    for (int i=0; i<3; i++ ) {
+		ecefVector[j] += temp[j][i] * enuVector[i];
+            }
+	}
+	
+    }
+	 
+    /**
+     * Set x.
+     * @param x in meters.
+     */
+    public void setX(float pX) {
+        x = pX;
+    }
+
+    /**
+     * Set y.
+     * @param y in meters.
+     */
+    public void setY(float pY) {
+        y = pY;
+    }
+    
+    /**
+     * Set z.
+     * @param z in meters.
+     */
+    public void setZ(float pZ) {
+        z = pZ;
+    }
+
+    /**
+     * Set x,y,z.
+     * @param v1 East.
+     * @param v2 North.
+     * @param v3 Up.
+     */
+    public void setXYZ(float v1, float v2, float v3) {
+         x = v1;
+         y = v2;
+         z = v3;
+    }
+
+    /**
+     * Get x.
+     * @return float x.
+     */
+    public float getX() {
+        return  x;
+    }
+
+    /**
+     * Get Y.
+     * @return float y.
+     */
+    public float getY() {
+        return  y;
+    }
+
+    /**
+     * Get Z.
+     * @return float z.
+     */
+    public float getZ() {
+        return  z;
+    }
+    
+    /**
+     * To get the direction as a vector.
+     *
+     * @param degrees is heading 0-360
+     * @param latitude in degrees.
+     * @param longitude in degrees.
+     * @param ecef vector
+     */
+    public void toDirectionVector(double degrees, double latitude, double longitude, double ecefVector[]) {
+   
+    	
+        double radians = Math.toRadians(degrees) - 360;
+        double east = Math.sin(radians);
+        double north = Math.cos(radians);
+        double lat_ = Math.toRadians(latitude);
+        double lon_ = Math.toRadians(longitude);
+        double up = 0.0;
+        double enuVector[] =  {
+            east, north, up
+        };
+        this.enu2ecef(ecefVector, lat_, lon_, enuVector);
+    }
+
+    /*
+    public static void main(String[] args) {
+
+        double ecefVector[] = new double[3];
+        double enuVector[] = new double[3];
+        double longitude = Math.toRadians(40.00);
+        double latitude = Math.toRadians(-74.50)
+    
+        EnuFrame test = new EnuFrame();
+    
+        ecefVector[0] = 13;
+        ecefVector[1] = 5;
+        ecefVector[2] = 8;
+    
+        enuVector[0] = 0.e0;
+        enuVector[0] = 0.e0;
+        enuVector[0] = 0.e0;
+    
+        System.err.println("lat" + latitude + " lon" + longitude);
+        //System.err.println("" + ecefVector[0] + ":" + ecefVector[2]+ ecefVector[2] );
+        //System.err.println("" + enuVector[0] + ":" + enuVector[2]+ enuVector[2] ); 
+    
+
+        // TEST1  
+        test.ecef2enu(ecefVector, latitude, longitude, enuVector);
+        System.err.println("TEST1:");
+        System.err.println("ECEF:" + ecefVector[0] + " : " + ecefVector[1]+ " :" +ecefVector[2] );
+        System.err.println("ENU:" + enuVector[0] + " : " + enuVector[1]+ " : "+enuVector[2] );
+
+        ecefVector[0] = 0.e0;
+        ecefVector[1] = 0.e0;
+        ecefVector[2] = 0.e0;
+    
+        // TEST2
+        test.enu2ecef(ecefVector, latitude, longitude, enuVector);
+        System.err.println("TEST2:");
+        System.err.println("ENU:" + enuVector[0] + " : " + enuVector[1]+ " : "+enuVector[2] );
+        System.err.println("ECEF:" + ecefVector[0] + " : " + ecefVector[1]+ " :" +ecefVector[2] ); 
+    }
+    */
+
+
+}//class
---
+++ src/openmap/com/bbn/openmap/proj/coords/NedFrame.java
@@ -0,0 +1,317 @@
+package  com.bbn.openmap.proj.coords;
+
+import com.bbn.openmap.LatLonPoint;
+import com.bbn.openmap.proj.ProjMath;
+
+/**
+ * Encapsulates the NED (North-East-Down) coordinate system.
+ * This is also know as local tangent plane (LTP).
+ * <p>
+ * Author: Robert Hayes
+ * <p>
+ */
+public class NedFrame {
+    
+    /**
+     * The x component of the coordinate (NORTH).
+     */
+    protected float x;
+    /**
+     * The y component of the coordinate (EAST).
+     */
+    protected float y;
+    /**
+     * The z component of the coordinate (DOWN).
+     */
+    protected float z;
+
+    /**
+     * Construct a default NedFrame.
+     * x,y,z are all set to zero.
+     */
+    public NedFrame() {
+    	x = 0.0f;
+    	y = 0.0f;
+    	z = 0.0f;
+    }
+
+    /**
+     * Construct a NedFrame from ECEF vector and latitude and longitude.
+     *
+     * @param x ecef x.
+     * @param y ecef y.
+     * @param z ecef z.
+     * @param lat = latitude in degrees.
+     * @param lon = longitude in degrees.
+     */
+    public NedFrame(float x, float y, float z, double lat, double lon) {
+        // All calculations are done using radians!
+
+        double ecef[] = new double[3];
+        double ned[] = new double[3];
+        
+        double latitude = ProjMath.degToRad(lat);
+        double longitude = ProjMath.degToRad(lon);
+        
+        ecef[0] = x;
+        ecef[1] = y;
+        ecef[2] = z;
+        
+        ecef2ned(ned, latitude, longitude, ecef);
+        
+        this.x = (float)ned[0];
+	this.y = (float)ned[1];
+	this.z = (float)ned[2];
+    }
+    
+    /**
+     * Construct a NedFrame from a ECEF vector and a LatLonPoint.
+     *
+     * @param ecefVector.
+     */
+    public NedFrame(double[] ecefVector, LatLonPoint llpt) {
+        // All calculations are done using radians!
+        
+        
+        double ecef[] = new double[3];
+        double ned[] = new double[3];
+        double lat_ = (double)llpt.getLatitude();
+        double lon_ = (double)llpt.getLongitude();
+        double latitude = ProjMath.degToRad(lat_);
+        double longitude = ProjMath.degToRad(lon_);
+        
+        ecef[0] = ecefVector[0];
+        ecef[1] = ecefVector[1];
+        ecef[2] = ecefVector[2];
+        
+        ecef2ned(ned, latitude, longitude, ecef);
+        
+        this.x = (float)ned[0];
+	this.y = (float)ned[1];
+	this.z = (float)ned[2];
+        
+    }
+    
+    /**
+     * Convert to  a geocentric frame using a LatLonPoint.
+     *
+     * @param llpt.
+     * @return a vector of ecef values
+     */
+    public double[] toGeocentricFrame(LatLonPoint llpt) {
+    	// All calculations are done using radians!
+        double ecef[] = new double[3];
+        double ned[] = new double[3];
+    	double lat_ = (double)llpt.getLatitude();
+        double lon_ = (double)llpt.getLongitude();
+        double latitude = ProjMath.degToRad(lat_);
+        double longitude = ProjMath.degToRad(lon_);
+        
+        ned2ecef(ned, latitude, longitude, ecef);      
+	    
+    	return ecef;
+    }
+    
+    /**
+     * Internal conversion routine.
+     *
+     * @param ned vector
+     * @param latitude in radians.
+     * @param longitude in radians
+     * @param ecef vector
+     */
+    public void ecef2ned(double ned[], double latitude, double longitude, double ecef[] ) {
+    	
+    	double temp[][] = new double[3][3];
+               
+    	double clat = Math.cos(latitude);
+	double clon = Math.cos(longitude);
+	double slat = Math.sin(latitude);
+	double slon = Math.sin(longitude);
+
+	temp[0][0] = -slat * clon;
+	temp[0][1] = -slat * slon;
+	temp[0][2] =  clat;
+
+	temp[1][0] = -slon;
+	temp[1][1] =  clon;
+	temp[1][2] = 0.0;
+
+	temp[2][0] = -clat * clon;
+	temp[2][1] = -clat * slon;
+	temp[2][2] = -slat;
+        
+        for(int j=0; j<3; ++j)
+	{
+	    ned[j] = 0.0;
+	    for(int i=0; i<3; i++ )
+		ned[j] += temp[j][i] * ecef[i];
+	}
+	    
+    }
+    
+    /**
+     * Internal conversion routine.
+     *
+     * @param ned vector
+     * @param latitude in radians.
+     * @param longitude in radians
+     * @param ecef vector
+     */
+    public void ned2ecef(double ned[], double latitude, double longitude, double ecef[]) {
+    	
+    	double temp[][] = new double[3][3];
+             
+    	double clat = Math.cos(latitude);
+	double clon = Math.cos(longitude);
+	double slat = Math.sin(latitude);
+	double slon = Math.sin(longitude);
+	    
+	ned[0] = this.x;
+        ned[1] = this.y;
+        ned[2] = this.z; 
+        
+    	temp[0][0] = -slat * clon;
+	temp[1][0] = -slat * slon;
+	temp[2][0] =  clat;
+
+	temp[0][1] = -slon;
+	temp[1][1] = clon;
+	temp[2][1] = 0.0;
+
+	temp[0][2] = -clat * clon;
+	temp[1][2] = -clat * slon;
+	temp[2][2] = -slat;
+	    
+	for(int j=0; j<3; ++j)
+	{
+	    ecef[j] = 0.0;
+	    for(int i=0; i<3; i++ )
+		ecef[j] += temp[j][i] * ned[i];
+	}
+    }
+       
+    /**
+     * Copy construct a NedFrame.
+     *
+     * @param pt NedFrame
+     */
+    public NedFrame(NedFrame nedpt) {
+        x = nedpt.x;
+        y = nedpt.y;
+        z = nedpt.z;
+    }
+
+    /**
+     * 
+     * @return String representation of NED vector.
+     */
+    public String toString() {
+        return  "NedFrame[N=" + x + ",E=" + y + ",D=" + z + "]";
+    }
+
+    /**
+     * Set x.
+     * @param x in meters.
+     */
+    public void setX(float pX) {
+        x = pX;
+    }
+
+    /**
+     * Set y.
+     * @param y in meters.
+     */
+    public void setY(float pY) {
+        y = pY;
+    }
+    
+    /**
+     * Set z.
+     * @param z in meters.
+     */
+    public void setZ(float pZ) {
+        z = pZ;
+    }
+
+    /**
+     * Set x,y,z.
+     * @param v1 North.
+     * @param v2 East.
+     * @param v3 Down.
+     */
+    public void setXYZ(float v1, float v2, float v3) {
+	x = v1;
+	y = v2;
+	z = v3;
+    }
+
+    /**
+     * Get x.
+     * @return float x.
+     */
+    public float getX() {
+        return  x;
+    }
+
+    /**
+     * Get Y.
+     * @return float y.
+     */
+    public float getY() {
+        return  y;
+    }
+
+    /**
+     * Get Z.
+     * @return float z.
+     */
+    public float getZ() {
+        return  z;
+    }
+    
+    /**
+     * Get speed.
+     * @return double speed.
+     */
+    public double toSpeed() {
+    	return Math.sqrt(x * x + y * y );
+    }
+    
+    /**
+     * Get heading.
+     * @return double heading in degrees.
+     */
+    public double toHeading() {
+    	double radians = Math.atan2(y,x);
+    	double degrees = Math.toDegrees(radians);
+    	if (degrees < 0 ) {
+    	    degrees += degrees + 360;
+        }
+        return degrees;   	
+    }
+    
+    /*
+      public final static void main (String[] args) {
+      // TEST1
+      double temp[] = new double[3];
+      temp[0] = 13;
+      temp[1] = 5;
+      temp[2] = 8;
+    	
+      LatLonPoint llpt = new LatLonPoint(40.00,-74.500);
+      System.out.println("llpt" + llpt);   	
+      System.out.println("ecef:" + temp[0] + " : " + temp[1] + " : " + temp[2]);
+    	
+      NedFrame nedv = new NedFrame(temp,llpt);
+      System.out.println("ned" + nedv);
+    	   	
+      temp = nedv.toGeocentricFrame(llpt);
+      System.out.println("ecef:" + temp[0] + " : " + temp[1] + " : " + temp[2]);   	
+      }
+    */
+
+}//class
+
+
+
