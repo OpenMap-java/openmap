e1de729ce02dc148ffd715e119322ec8a90c4675
dietrick <dietrick@f68c23cc-7f87-4fbc-acfa-d9760b90acc1>
2003-06-02 18:48:14 +0000

    Updated ANT_ENV.bat with latest/current versions
    git-svn-id: svn+ssh://src.dsl.bbn.com/svn/openmap/trunk@740 f68c23cc-7f87-4fbc-acfa-d9760b90acc1

--- ANT_ENV.bat
+++ ANT_ENV.bat
@@ -2,9 +2,10 @@ rem These environment variables are required so that Ant works correctly in Dos.
 rem If you have set them, then there is no need to run this file.
 rem Otherwise update the paths to reflect your environment.
 
-set OPENMAP_HOME=C:\openmap-4.5.1
-set ANT_HOME=jakarta-ant-1.4.1
-set JAVA_HOME=C:\jdk1.3.1_01
+set OPENMAP_HOME=C:\openmap
+set JAVA_HOME=C:\jdk1.4.1_01
+
+set ANT_HOME=%OPENMAP_HOME%\ext\jakarta-ant-1.5.1
 set PATH=%ANT_HOME%\bin;%PATH%
 set CLASSPATH=%CLASSPATH%;%ANT_HOME%\lib\ant.jar;%ANT_HOME%\lib\crimson.jar;%ANT_HOME%\lib\jaxp.jar
 
