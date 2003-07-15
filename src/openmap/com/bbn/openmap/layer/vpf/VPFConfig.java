01ca8bbd67029f65b5b452963074c22c91c26a98
dietrick <dietrick@f68c23cc-7f87-4fbc-acfa-d9760b90acc1>
2003-07-15 23:54:11 +0000

    Added copyright header
    git-svn-id: svn+ssh://src.dsl.bbn.com/svn/openmap/trunk@765 f68c23cc-7f87-4fbc-acfa-d9760b90acc1

--- src/openmap/com/bbn/openmap/layer/vpf/VPFConfig.java
+++ src/openmap/com/bbn/openmap/layer/vpf/VPFConfig.java
@@ -1,3 +1,26 @@
+// **********************************************************************
+// 
+// <copyright>
+// 
+//  BBN Technologies, a Verizon Company
+//  10 Moulton Street
+//  Cambridge, MA 02138
+//  (617) 873-8000
+// 
+//  Copyright (C) BBNT Solutions LLC. All rights reserved.
+// 
+// </copyright>
+// **********************************************************************
+// 
+// $Source: /cvs/distapps/openmap/src/openmap/com/bbn/openmap/layer/vpf/VPFConfig.java,v $
+// $RCSfile: VPFConfig.java,v $
+// $Revision: 1.5 $
+// $Date: 2003/07/15 23:54:11 $
+// $Author: dietrick $
+// 
+// **********************************************************************
+
+
 package com.bbn.openmap.layer.vpf;
 
 import java.awt.*;
