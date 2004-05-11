82fa1d2071b0bb98444b930d4f09bba3d0af6846
dietrick <dietrick@f68c23cc-7f87-4fbc-acfa-d9760b90acc1>
2004-05-11 23:45:52 +0000

    Updated/simplified ANT_ENV.bat, removed bin/ant.bat because it's unnecessary and didn't work
    git-svn-id: svn+ssh://src.dsl.bbn.com/svn/openmap/trunk@1180 f68c23cc-7f87-4fbc-acfa-d9760b90acc1

--- ANT_ENV.bat
+++ ANT_ENV.bat
@@ -2,12 +2,14 @@ rem These environment variables are required so that Ant works correctly in Dos.
 rem If you have set them, then there is no need to run this file.
 rem Otherwise update the paths to reflect your environment.
 
+rem Set OPENMAP_HOME to the top-level openmap directory, the one
+rem containing the build.xml file.
 set OPENMAP_HOME=C:\openmap
-set JAVA_HOME=C:\jdk1.4.1_01
+set JAVA_HOME=C:\j2sdk1.4.2_03
 
+rem You only need these if you don't have ant configured to run on your system.
 set ANT_HOME=%OPENMAP_HOME%\ext\apache-ant-1.6.1
 set PATH=%ANT_HOME%\bin;%PATH%
-set CLASSPATH=%CLASSPATH%;%ANT_HOME%\lib\ant.jar;%ANT_HOME%\lib\crimson.jar;%ANT_HOME%\lib\jaxp.jar
 
 rem This is optional if you already have java.exe in your path
 rem set PATH=%JAVA_HOME%\bin;%PATH%
