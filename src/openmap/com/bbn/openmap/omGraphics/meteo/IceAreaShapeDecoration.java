877e05077ed508626abadcc0902b02247fff34fe
dietrick <dietrick@f68c23cc-7f87-4fbc-acfa-d9760b90acc1>
2004-09-18 19:43:39 +0000

    Adding IceArea and Turbulance shape decorations
    git-svn-id: svn+ssh://src.dsl.bbn.com/svn/openmap/trunk@1218 f68c23cc-7f87-4fbc-acfa-d9760b90acc1

---
+++ src/openmap/com/bbn/openmap/omGraphics/meteo/IceAreaShapeDecoration.java
@@ -0,0 +1,92 @@
+package com.bbn.openmap.omGraphics.meteo;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Point2D;
+
+import com.bbn.openmap.omGraphics.awt.AbstractShapeDecoration;
+
+/**
+ * A ShapeDecoration for meteorological icy conditions.
+ */
+public class IceAreaShapeDecoration extends AbstractShapeDecoration {
+
+    /** We use to draw them red ! */
+    public static Color COLOR = new Color(102, 51, 0);
+
+    /** Filled half circles for surface fronts, 
+     * empty ones for altitude fronts
+     */ 
+    private boolean filled = false;
+
+    /**
+     * Constructor.
+     * @param length
+     * @param width
+     * @param orientation
+     */
+    public IceAreaShapeDecoration(int length, int width, int orientation) {
+        super(length, width, orientation);
+        setPaint(COLOR);
+    }
+
+    /**
+     * @see com.bbn.openmap.omGraphics.awt.ShapeDecoration#draw(Graphics, Point2D[], boolean)
+     */
+    public void draw(Graphics g, Point2D[] points, boolean complete) {
+        Graphics2D g2D = (Graphics2D) g;
+        setGraphics(g);
+
+        int nbpts = points.length;
+
+        double xcoord1 = points[0].getX();
+        double ycoord1 = points[0].getY();
+        double xcoord2 = points[nbpts - 1].getX();
+        double ycoord2 = points[nbpts - 1].getY();
+
+        g.drawLine((int)xcoord1, (int)ycoord1, (int)xcoord2, (int)ycoord2);
+
+        if (complete) {
+            int orient = getOrientation() == LEFT ? -1 : 1;
+
+            // Compute cosinus and sinus of rotation angle
+            double dx = xcoord2 - xcoord1;
+            double dy = ycoord2 - ycoord1;
+            double norm = Math.sqrt(dx * dx + dy * dy);
+            double rcos = dx / norm;
+            double rsin = dy / norm;
+
+            // Compute vertices
+            double r = getLength() / 2.0; // x radius before rotation
+            double w = orient * getWidth(); // y radius before rotation
+            // rotate 
+
+            int x2 = (int) (xcoord1 + r * rcos);
+            int y2 = (int) (ycoord1 + r * rsin);
+            int x1 = (int) (x2 - w * rsin);
+            int y1 = (int) (y2 + w * rcos);
+
+            g.drawLine((int)x2, (int)y2, (int)x1, (int)y1);
+        }
+
+        restoreGraphics(g);
+    }
+
+    /**
+     * Returns the filled boolean.
+     * @return boolean
+     */
+    public boolean isFilled() {
+        return filled;
+    }
+
+    /**
+     * Sets the filled (draw a half disk or a half circle ?).
+     * @param filled The filled to set
+     */
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+}
+
---
+++ src/openmap/com/bbn/openmap/omGraphics/meteo/TurbulanceShapeDecoration.java
@@ -0,0 +1,89 @@
+package com.bbn.openmap.omGraphics.meteo;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Point2D;
+
+import com.bbn.openmap.omGraphics.awt.AbstractShapeDecoration;
+
+/**
+ * A ShapeDecoration for meteorological turbulance conditions.
+ */
+public class TurbulanceShapeDecoration extends AbstractShapeDecoration {
+
+    /** We use to draw them red ! */
+    public static Color COLOR = Color.blue;
+
+    /** Filled half circles for surface fronts, 
+     * empty ones for altitude fronts
+     */ 
+    private boolean filled = true;
+
+    /**
+     * Constructor.
+     * @param length
+     * @param radius
+     * @param orientation
+     */
+    public TurbulanceShapeDecoration(int length, int radius) {
+        super(length, radius, 1);
+        setPaint(COLOR);
+    }
+
+    /**
+     * @see com.bbn.openmap.omGraphics.awt.ShapeDecoration#draw(Graphics, Point2D[], boolean)
+     */
+    public void draw(Graphics g, Point2D[] points, boolean complete) {
+        Graphics2D g2D = (Graphics2D) g;
+        setGraphics(g);
+
+        int nbpts = points.length;
+
+        double xcoord1 = points[0].getX();
+        double ycoord1 = points[0].getY();
+        double xcoord2 = points[nbpts - 1].getX();
+        double ycoord2 = points[nbpts - 1].getY();
+
+        if (complete) {
+
+            // Compute cosinus and sinus of rotation angle
+            double dx = xcoord2 - xcoord1;
+            double dy = ycoord2 - ycoord1;
+            double norm = Math.sqrt(dx * dx + dy * dy);
+            double rcos = dx / norm;
+            double rsin = dy / norm;
+
+            // Compute vertices
+            double r = getLength() / 2.0; // x radius before rotation
+            double w = getWidth();
+            // rotate 
+
+            int x = (int) (xcoord1 + r * rcos);
+            int y = (int) (ycoord1 + r * rsin);
+
+            g.fillOval((int)(x - w/2), (int)(y - w/2), 
+                       (int)(w), (int)(w));
+        }
+
+        restoreGraphics(g);
+
+    }
+
+    /**
+     * Returns the filled boolean.
+     * @return boolean
+     */
+    public boolean isFilled() {
+        return filled;
+    }
+
+    /**
+     * Sets the filled (draw a half disk or a half circle ?).
+     * @param filled The filled to set
+     */
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+}
+
