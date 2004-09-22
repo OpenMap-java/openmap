ca39793362054f36525047ec2be6e69c51d88507
dietrick <dietrick@f68c23cc-7f87-4fbc-acfa-d9760b90acc1>
2004-09-22 20:46:48 +0000

    Adding OMScalingRaster editing.
    git-svn-id: svn+ssh://src.dsl.bbn.com/svn/openmap/trunk@1230 f68c23cc-7f87-4fbc-acfa-d9760b90acc1

---
+++ src/openmap/com/bbn/openmap/tools/drawing/OMScalingRasterLoader.java
@@ -0,0 +1,75 @@
+// **********************************************************************
+// 
+// <copyright>
+// 
+//  BBN Technologies, a Verizon Company
+//  10 Moulton Street
+//  Cambridge, MA 02138
+//  (617) 873-8000
+// 
+//  Copyright (C) BBNT Solutions LLC. All rights reserved.
+// 
+// </copyright>
+// **********************************************************************
+// 
+// $Source: /cvs/distapps/openmap/src/openmap/com/bbn/openmap/tools/drawing/OMScalingRasterLoader.java,v $
+// $RCSfile: OMScalingRasterLoader.java,v $
+// $Revision: 1.1 $
+// $Date: 2004/09/22 20:46:48 $
+// $Author: dietrick $
+// 
+// **********************************************************************
+
+
+package com.bbn.openmap.tools.drawing;
+
+import com.bbn.openmap.omGraphics.EditableOMGraphic;
+import com.bbn.openmap.omGraphics.EditableOMScalingRaster;
+import com.bbn.openmap.omGraphics.GraphicAttributes;
+import com.bbn.openmap.omGraphics.OMGraphic;
+import com.bbn.openmap.omGraphics.OMScalingRaster;
+
+/**
+ * Loader that knows how to create/edit OMRect objects.
+ */
+public class OMScalingRasterLoader extends AbstractToolLoader implements EditToolLoader {
+
+    protected String graphicClassName = "com.bbn.openmap.omGraphics.OMScalingRaster";
+
+    public OMScalingRasterLoader() {
+        init();
+    }
+
+    public void init() {
+        EditClassWrapper ecw = 
+            new EditClassWrapper(graphicClassName,
+                                 "com.bbn.openmap.omGraphics.EditableOMScalingRaster",
+                                 "editablescalingraster.gif",
+                                 i18n.get(OMScalingRasterLoader.class,"omscalingraster","Raster"));
+        addEditClassWrapper(ecw);
+    }
+
+    /**
+     * Give the classname of a graphic to create, returning an
+     * EditableOMGraphic for that graphic.  The GraphicAttributes
+     * object lets you set some of the initial parameters of the rect,
+     * like rect type and rendertype.
+     */
+    public EditableOMGraphic getEditableGraphic(String classname, GraphicAttributes ga) {
+        if (classname.intern() == graphicClassName) {
+            return new EditableOMScalingRaster(ga);
+        }
+        return null;
+    }
+
+    /**
+     * Give an OMGraphic to the EditToolLoader, which will create an
+     * EditableOMGraphic for it.
+     */
+    public EditableOMGraphic getEditableGraphic(OMGraphic graphic) {
+        if (graphic instanceof OMScalingRaster) {
+            return new EditableOMScalingRaster((OMScalingRaster)graphic);
+        }
+        return null;
+    }
+}
