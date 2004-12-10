74475c89ef2d1b65c333b28153a9d06c9f0ef838
dietrick <dietrick@f68c23cc-7f87-4fbc-acfa-d9760b90acc1>
2004-12-10 14:09:31 +0000

    Moved DimensionQueryPanel from SaveAsImageFileChooser to its own class, which is now used by the SymbolChooser as well.
    git-svn-id: svn+ssh://src.dsl.bbn.com/svn/openmap/trunk@1309 f68c23cc-7f87-4fbc-acfa-d9760b90acc1

--- src/openmap/com/bbn/openmap/gui/menu/SaveAsImageFileChooser.java
+++ src/openmap/com/bbn/openmap/gui/menu/SaveAsImageFileChooser.java
@@ -14,17 +14,20 @@
 // 
 // $Source: /cvs/distapps/openmap/src/openmap/com/bbn/openmap/gui/menu/SaveAsImageFileChooser.java,v $
 // $RCSfile: SaveAsImageFileChooser.java,v $
-// $Revision: 1.4 $
-// $Date: 2004/10/14 18:05:50 $
+// $Revision: 1.5 $
+// $Date: 2004/12/10 14:08:54 $
 // $Author: dietrick $
 // 
 // **********************************************************************
 
 package com.bbn.openmap.gui.menu;
 
-import java.awt.*;
-import javax.swing.*;
+import java.awt.BorderLayout;
 
+import javax.swing.JFileChooser;
+import javax.swing.JPanel;
+
+import com.bbn.openmap.gui.DimensionQueryPanel;
 import com.bbn.openmap.util.PaletteHelper;
 
 /**
@@ -33,15 +36,15 @@ import com.bbn.openmap.util.PaletteHelper;
  */
 public class SaveAsImageFileChooser extends JFileChooser {
 
-    SaveAsImageFileChooser.DimensionQueryPanel dqp = new SaveAsImageFileChooser.DimensionQueryPanel();
+    DimensionQueryPanel dqp = new DimensionQueryPanel();
 
     /**
      * Create file chooser with the image size fields filled in.
      */
     public SaveAsImageFileChooser(int width, int height) {
         super();
-        dqp.setImageHeight(height);
-        dqp.setImageWidth(width);
+        dqp.setHeight(height);
+        dqp.setWidth(width);
         JPanel imageSizePanel = PaletteHelper.createPaletteJPanel(" Set Image Size ");
         imageSizePanel.setLayout(new BorderLayout());
         imageSizePanel.add(dqp, BorderLayout.CENTER);
@@ -52,126 +55,28 @@ public class SaveAsImageFileChooser extends JFileChooser {
      * Set the value of the image width setting from the GUI.
      */
     public void setImageWidth(int w) {
-        dqp.setImageWidth(w);
+        dqp.setWidth(w);
     }
 
     /**
      * Get the value of the image width setting from the GUI.
      */
     public int getImageWidth() {
-        return dqp.getImageWidth();
+        return dqp.getWidth();
     }
 
     /**
      * Set the value of the image height setting from the GUI.
      */
     public void setImageHeight(int h) {
-        dqp.setImageHeight(h);
+        dqp.setHeight(h);
     }
 
     /**
      * Get the value of the image height setting from the GUI.
      */
     public int getImageHeight() {
-        return dqp.getImageHeight();
+        return dqp.getHeight();
     }
 
-    public class DimensionQueryPanel extends JPanel {
-
-        private JTextField hfield;
-        private JTextField vfield;
-        private JLabel htext;
-        private JLabel vtext;
-        private JLabel ptext1;
-        private JLabel ptext2;
-
-        public DimensionQueryPanel() {
-            this(0, 0);
-        }
-
-        public DimensionQueryPanel(int width, int height) {
-
-            htext = new JLabel("Width: ");
-            htext.setHorizontalAlignment(SwingConstants.RIGHT);
-            vtext = new JLabel("Height: ");
-            vtext.setHorizontalAlignment(SwingConstants.RIGHT);
-            hfield = new JTextField(Integer.toString(width), 5);
-            vfield = new JTextField(Integer.toString(height), 5);
-            ptext1 = new JLabel(" pixels");
-            ptext2 = new JLabel(" pixels");
-            layoutPanel();
-        }
-
-        public void setImageWidth(int width) {
-            hfield.setText(Integer.toString(width));
-        }
-
-        public int getImageWidth() {
-            return Integer.parseInt(hfield.getText());
-        }
-
-        public void setImageHeight(int height) {
-            vfield.setText(Integer.toString(height));
-        }
-
-        public int getImageHeight() {
-            return Integer.parseInt(vfield.getText());
-        }
-
-        public void layoutPanel() {
-
-            GridBagLayout gb = new GridBagLayout();
-            GridBagConstraints c = new GridBagConstraints();
-            setLayout(gb);
-
-            c.insets = new Insets(3, 3, 3, 3);
-            c.gridx = 0;
-            c.gridy = 0;
-            c.fill = GridBagConstraints.NONE;
-            c.weightx = 0;
-            c.anchor = GridBagConstraints.EAST;
-
-            gb.setConstraints(htext, c);
-            add(htext);
-
-            c.gridx = 1;
-            c.fill = GridBagConstraints.HORIZONTAL;
-            c.weightx = 1f;
-            c.anchor = GridBagConstraints.WEST;
-
-            gb.setConstraints(hfield, c);
-            add(hfield);
-
-            c.gridx = 2;
-            c.fill = GridBagConstraints.NONE;
-            c.weightx = 0;
-            gb.setConstraints(ptext1, c);
-            add(ptext1);
-
-            // Next row
-
-            c.gridx = 0;
-            c.gridy = 1;
-            c.fill = GridBagConstraints.NONE;
-            c.weightx = 0;
-            c.anchor = GridBagConstraints.EAST;
-
-            gb.setConstraints(vtext, c);
-            add(vtext);
-
-            c.gridx = 1;
-            c.fill = GridBagConstraints.HORIZONTAL;
-            c.weightx = 1f;
-            c.anchor = GridBagConstraints.WEST;
-
-            gb.setConstraints(vfield, c);
-            add(vfield);
-
-            c.gridx = 2;
-            c.fill = GridBagConstraints.NONE;
-            c.weightx = 0;
-            gb.setConstraints(ptext2, c);
-            add(ptext2);
-        }
-    }
 }
