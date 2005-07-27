e5e3a564b0c3e48a670b0937cac3e6b66c515086
dietrick <dietrick@f68c23cc-7f87-4fbc-acfa-d9760b90acc1>
2005-07-27 17:25:07 +0000

    Fixed the BoundaryCrossing .in and .out pointers for regions, they were getting a little mixed up in some situations.
    git-svn-id: svn+ssh://src.dsl.bbn.com/svn/openmap/trunk@1441 f68c23cc-7f87-4fbc-acfa-d9760b90acc1

--- src/openmap/com/bbn/openmap/geo/BoundaryCrossing.java
+++ src/openmap/com/bbn/openmap/geo/BoundaryCrossing.java
@@ -16,8 +16,8 @@
 ///cvs/darwars/ambush/aar/src/com/bbn/ambush/mission/MissionHandler.java,v
 //$
 //$RCSfile: BoundaryCrossing.java,v $
-//$Revision: 1.2 $
-//$Date: 2005/07/22 21:22:48 $
+//$Revision: 1.3 $
+//$Date: 2005/07/27 17:23:49 $
 //$Author: dietrick $
 //
 //**********************************************************************
@@ -69,13 +69,13 @@ public class BoundaryCrossing {
         List crossings = new ArrayList(10);
         List lastSegmentCrossingList;
         Geo lastSegmentStartingPoint;
-        
+
         public Collector() {}
 
         protected void addCrossing(BoundaryCrossing bc) {
             crossings.add(bc);
         }
-        
+
         protected void addCrossing(Collection c, GeoSegment segment,
                                    GeoRegion region) {
 
@@ -99,19 +99,32 @@ public class BoundaryCrossing {
                 lastSegmentCrossingList = new ArrayList(10);
             }
 
+            // The ordered list is for temporarily holding points for
+            // the current segment as they are placed in the right
+            // order.
             LinkedList orderedList = new LinkedList();
-
+            // Everything in the lastSegmentCrossingList has already
+            // be ordered relative to what's already been searched, so
+            // we can just add them now to place the new points around
+            // them accordingly. Also, the lastSegmentCrossingList
+            // holds BoundaryCrossing objects, and the collection
+            // passed into this function doesn't have those yet.
             orderedList.addAll(lastSegmentCrossingList);
+            // Clear out the lastSegmetnCrossingList, we'll replenish
+            // it with the ordered list at the end, so it will be
+            // ready for the next cycle.
             lastSegmentCrossingList.clear();
 
             for (Iterator it = c.iterator(); it.hasNext();) {
                 Geo current = (Geo) it.next();
                 double curDist = start.distance(current);
+                // We just assume that crossing point is going into
+                // the current region, we'll check later to make sure.
                 BoundaryCrossing currentBC = new BoundaryCrossing(current, region, true);
 
                 int lastCheckedIndex = 0;
-
                 BoundaryCrossing lastChecked = null;
+
                 for (Iterator it2 = orderedList.iterator(); it2.hasNext(); lastCheckedIndex++) {
                     lastChecked = (BoundaryCrossing) it2.next();
                     if (curDist < start.distance(lastChecked.geo)) {
@@ -133,14 +146,21 @@ public class BoundaryCrossing {
             for (Iterator it = orderedList.iterator(); it.hasNext();) {
                 BoundaryCrossing bc = (BoundaryCrossing) it.next();
 
-                if (region == bc.in && !goinin) {
-                    bc.out = bc.in;
-                    bc.in = null;
+                boolean sameRegion = (bc.in == region);
+
+                if (sameRegion) {
+                    if (!goinin)
+                        bc.out = bc.in;
+                    goinin = !goinin;
                 }
 
                 lastSegmentCrossingList.add(bc);
-                goinin = !goinin;
             }
+
+            // OK, remember at this point that the BoundaryCrossing
+            // Objects have be.in set to their region, and some have
+            // region.out set if the boundary reflects an outward
+            // motion. We're going to resolve this in compact();
 
             lastSegmentStartingPoint = start;
         }
@@ -161,14 +181,28 @@ public class BoundaryCrossing {
             for (int i = 0; i < bc.length; i++) {
                 current = (BoundaryCrossing) bc[i];
 
-                if (previous != null && previous.geo.distance(current.geo) == 0.0) {
-                    if (previous.in == null)
+                if (previous != null
+                        && previous.geo.distance(current.geo) == 0.0) {
+                    // If the distances between points are zero, it's
+                    // a border crossing and we want to merge these
+                    // into one point, with the bc.out pointer marking
+                    // the region being left, and the bc.in marking
+                    // the receiving region.
+                    if (previous.out != null)
                         previous.in = current.in;
-                    if (previous.out == null) {
+                    if (current.out != null) {
                         previous.out = current.out;
                     }
-                    
+
                     continue;
+                } else {
+                    // First point or if the distance between points is not zero, if the bc.out is set, then we
+                    // should set the bc.in to null, just to indicate
+                    // that the point reflects a crossing from outside
+                    // any other region into that particular region.
+                    if (current.out != null) {
+                        current.in = null;
+                    }
                 }
 
                 crossings.add(current);
@@ -205,13 +239,14 @@ public class BoundaryCrossing {
          * the path, in case the path is kinda crazy, and then we can
          * keep track of the crossings in the right order.
          */
-        public boolean considerSegmentXRegion(GeoSegment segment, GeoRegion region) {
+        public boolean considerSegmentXRegion(GeoSegment segment,
+                                              GeoRegion region) {
             List hits = Intersection.segmentNearPoly(segment,
                     region.toPointArray(),
                     0.0);
 
             if (hits != null) {
-                ((Collector)collector).addCrossing(hits, segment, region);
+                ((Collector) collector).addCrossing(hits, segment, region);
                 return true;
             }
 
