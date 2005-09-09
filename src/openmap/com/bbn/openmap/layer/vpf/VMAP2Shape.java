3e19fd5db27c1e2d3699c6e441d27df57bf54457
dietrick <dietrick@f68c23cc-7f87-4fbc-acfa-d9760b90acc1>
2005-09-09 21:23:07 +0000

    Refactoring VMAP2Shape, moving thinning code as an option.
    git-svn-id: svn+ssh://src.dsl.bbn.com/svn/openmap/trunk@1508 f68c23cc-7f87-4fbc-acfa-d9760b90acc1

--- src/openmap/com/bbn/openmap/layer/vpf/VMAP2Shape.java
+++ src/openmap/com/bbn/openmap/layer/vpf/VMAP2Shape.java
@@ -14,8 +14,8 @@
 // 
 // $Source: /cvs/distapps/openmap/src/openmap/com/bbn/openmap/layer/vpf/VMAP2Shape.java,v $
 // $RCSfile: VMAP2Shape.java,v $
-// $Revision: 1.6 $
-// $Date: 2005/08/09 19:29:39 $
+// $Revision: 1.7 $
+// $Date: 2005/09/09 21:21:17 $
 // $Author: dietrick $
 // 
 // **********************************************************************
@@ -45,22 +45,48 @@ import com.bbn.openmap.util.PropUtils;
  */
 public class VMAP2Shape {
 
-    protected static String vmaptype = "bnd";
-    protected static String propsFileName = System.getProperty("user.home")
+    protected static final String DEF_VMAP_TYPE = "bnd";
+    protected static final String DEF_PROPS_FILE_NAME = System.getProperty("user.home")
             + System.getProperty("file.separator") + "openmap.properties";
-    protected static String prefix = "vmapref";
-    protected static boolean doThinning = false;
-    protected static float fan_eps = 0.1f;
-    protected static float zero_eps = 0.0001f;
-    protected static float threshold = 0.5f;
+    protected static final String DEF_PREFIX = "vmapref";
+    protected static final boolean DEF_DO_THINNING = false;
+    protected static final float DEF_FAN_EPS = 0.1f;
+    protected static final float DEF_ZERO_EPS = 0.0001f;
+    protected static final float DEF_THRESHOLD = 0.5f;
+
+    protected String vmaptype = DEF_VMAP_TYPE;
+    protected String propsFileName = DEF_PROPS_FILE_NAME;
+    protected String prefix = DEF_PREFIX;
+    protected boolean doThinning = DEF_DO_THINNING;
+    protected float fan_eps = DEF_FAN_EPS;
+    protected float zero_eps = DEF_ZERO_EPS;
+    protected float threshold = DEF_THRESHOLD;
 
     protected LibrarySelectionTable lst;
     protected transient LayerGraphicWarehouseSupport warehouse;
 
     public VMAP2Shape() {}
 
+    /**
+     * Will load the properties set in the VPF2Shape object, fetch the
+     * OMGraphics from the VPF database, and write the shape file.
+     * 
+     * @param shapeFileName
+     */
+    public void writeShapeFile(String shapeFileName) {
+        setProperties(prefix, loadProperties());
+        writeShapeFile(shapeFileName, getRectangle());
+    }
+
+    /**
+     * Write the shape file, assumes that the properties have been
+     * loaded and the graphics fetched from the VPF database.
+     * 
+     * @param shapeFileName the file name to write the shapes into.
+     * @param graphics OMGraphics from VPF database to write to shape
+     *        file.
+     */
     public void writeShapeFile(String shapeFileName, OMGraphicList graphics) {
-        OMGraphicList saveGraphics = new OMGraphicList();
         try {
             ShapeFile s = new ShapeFile(shapeFileName);
             int nGraphics = graphics.size();
@@ -77,61 +103,79 @@ public class VMAP2Shape {
 
             System.out.println(nGraphics + " candidates.");
 
-            for (int i = 0; i < nGraphics; i++) {
-                OMGraphic omg = graphics.getOMGraphicAt(i);
-                if ((omg instanceof OMPoly)
-                        && (omg.getRenderType() == OMGraphic.RENDERTYPE_LATLON)) {
-                    OMPoly poly = (OMPoly) omg;
+            if (doThinning) {
+                OMGraphicList saveGraphics = new OMGraphicList();
+                for (int i = 0; i < nGraphics; i++) {
+                    OMGraphic omg = graphics.getOMGraphicAt(i);
+                    if ((omg instanceof OMPoly)
+                            && (omg.getRenderType() == OMGraphic.RENDERTYPE_LATLON)) {
+                        OMPoly poly = (OMPoly) omg;
 
-                    if (doThinning && maybeThrowAwayPoly(poly)) {
-                        continue;
+                        if (maybeThrowAwayPoly(poly)) {
+                            continue;
+                        }
+
+                        saveGraphics.addOMGraphic(poly);
+                    } else {
+                        System.out.println("Skipping candidate: "
+                                + omg.getClass().toString() + ", "
+                                + omg.getRenderType());
                     }
+                }
+                
+                graphics = saveGraphics;
 
-                    saveGraphics.addOMGraphic(poly);
-                } else {
-                    System.out.println("Skipping candidate: "
-                            + omg.getClass().toString() + ", "
-                            + omg.getRenderType());
+                // join polylines
+                if (false) {
+                    nGraphics = graphics.size();
+                    System.out.println("Joining " + nGraphics + " polyline candidates.");
+                    graphics = joinCommonLines(graphics, zero_eps);
                 }
             }
-            graphics = saveGraphics;
 
-            // join polylines
-            if (doThinning) {
-                nGraphics = graphics.size();
-                System.out.println(nGraphics + " candidates.");
-                graphics = joinCommonLines(graphics);
-            }
-
-            // save graphics
-            EsriShapeExport ese = new EsriShapeExport(graphics, (Projection)null, shapeFileName);
+            // Using com.bbn.openmap.dataAccess.shape package to write
+            // shape file:
+            EsriShapeExport ese = new EsriShapeExport(graphics, (Projection) null, shapeFileName);
             ese.export();
-            
-//            nGraphics = graphics.size();
-//            System.out.println("Dumping " + nGraphics + " graphics.");
-//            for (int i = 0; i < nGraphics; i++) {
-//                OMPoly poly = (OMPoly) graphics.getOMGraphicAt(i);
-//                float[] radians = poly.getLatLonArray();
-//                ESRIPolygonRecord epr = new ESRIPolygonRecord();
-//                epr.add(radians);
-//                epr.setPolygon(poly.isPolygon());//set POLYGON vs ARC
-//                s.add(epr);
-//                ++nDumped;
-//            }
-//
-//            s.verify(true, true);
-//            s.verify(true, true);
-//            s.close();
-//            System.out.println("Wrote " + nDumped + " Graphics.");
+
+            // Instead of using com.bbn.openmap.layer.shape package to
+            // write shape file:
+
+            // int nDumped = 0;
+            // nGraphics = graphics.size();
+            // System.out.println("Dumping " + nGraphics + "
+            // graphics.");
+            // for (int i = 0; i < nGraphics; i++) {
+            // OMPoly poly = (OMPoly) graphics.getOMGraphicAt(i);
+            // float[] radians = poly.getLatLonArray();
+            // ESRIPolygonRecord epr = new ESRIPolygonRecord();
+            // epr.add(radians);
+            // epr.setPolygon(poly.isPolygon());//set POLYGON vs ARC
+            // s.add(epr);
+            // ++nDumped;
+            // }
+            //
+            // s.verify(true, true);
+            // s.verify(true, true);
+            // s.close();
+            // System.out.println("Wrote " + nDumped + " Graphics.");
+
         } catch (java.io.IOException e) {
             e.printStackTrace();
         }
     }
 
-    // iterates through graphic list finding non-connected polylines.
-    // iterates over these to find lines with common endpoints and
-    // joining them.
-    protected static OMGraphicList joinCommonLines(OMGraphicList list) {
+    /**
+     * Iterates through graphic list finding non-connected polylines.
+     * iterates over these to find lines with common endpoints and
+     * joining them.
+     * 
+     * @param list
+     * @param zero_eps
+     * @return
+     */
+    protected static OMGraphicList joinCommonLines(OMGraphicList list,
+                                                   float zero_eps) {
         int size = list.size();
         int len1, len2;
         float lat1, lon1, lat2, lon2;
@@ -187,14 +231,14 @@ public class VMAP2Shape {
 
                 if (MoreMath.approximately_equal(lat1, lat2, zero_eps)
                         && MoreMath.approximately_equal(lon1, lon2, zero_eps)) {
-                    //                  System.out.println("joining...");
+                    // System.out.println("joining...");
                     radians = new float[len1 + len2 - 2];
                     System.arraycopy(rads1, 0, radians, 0, len1);
                     System.arraycopy(rads2, 0, radians, len1 - 2, len2);
                     poly1.setLocation(radians, OMGraphic.RADIANS);
                     plineGraphics.setOMGraphicAt(SinkGraphic.getSharedInstance(),
                             j);
-                    j = -1;//redo search
+                    j = -1;// redo search
                 }
             }
         }
@@ -210,7 +254,7 @@ public class VMAP2Shape {
         return newGraphics;
     }
 
-    // traverse array and coalesce adjacent points which are the same
+    /** traverse array and coalesce adjacent points which are the same */
     public static float[] coalesce_points(float[] radians, float eps,
                                           boolean ispolyg) {
         int write = 2;
@@ -232,21 +276,21 @@ public class VMAP2Shape {
         if (ispolyg && (write == 6)
                 && MoreMath.approximately_equal(radians[0], radians[4], eps)
                 && MoreMath.approximately_equal(radians[1], radians[5], eps)) {
-            write -= 2;//eliminate wrapped vertex
+            write -= 2;// eliminate wrapped vertex
         }
         float[] newrads = new float[write];
         System.arraycopy(radians, 0, newrads, 0, write);
         return newrads;
     }
 
-    // return true if we should throw away the poly
+    /** return true if we should throw away the poly */
     protected boolean maybeThrowAwayPoly(OMPoly poly) {
         float[] radians = poly.getLatLonArray();
         float lat, lon, thresh = ProjMath.degToRad(threshold);
         radians = coalesce_points(radians, 0.0001f, poly.isPolygon());
-        poly.setLocation(radians, OMGraphic.RADIANS);//install new
+        poly.setLocation(radians, OMGraphic.RADIANS);// install new
         if (radians.length < 4) {
-            return true;//throw away
+            return true;// throw away
         }
         if (poly.isPolygon() && (radians.length < 6)) {
             return true;
@@ -286,7 +330,7 @@ public class VMAP2Shape {
         }
 
         if (poly.isPolygon()) {
-            return true;//throw away
+            return true;// throw away
         }
 
         // throw away polyline if it's connected (island)
@@ -380,17 +424,89 @@ public class VMAP2Shape {
         return warehouse.getGraphics();
     }
 
+    public boolean isDoThinning() {
+        return doThinning;
+    }
+
+    public void setDoThinning(boolean doThinning) {
+        this.doThinning = doThinning;
+    }
+
+    public float getFan_eps() {
+        return fan_eps;
+    }
+
+    public void setFan_eps(float fan_eps) {
+        this.fan_eps = fan_eps;
+    }
+
+    public LibrarySelectionTable getLst() {
+        return lst;
+    }
+
+    public void setLst(LibrarySelectionTable lst) {
+        this.lst = lst;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public String getPropsFileName() {
+        return propsFileName;
+    }
+
+    public void setPropsFileName(String propsFileName) {
+        this.propsFileName = propsFileName;
+    }
+
+    public float getThreshold() {
+        return threshold;
+    }
+
+    public void setThreshold(float threshold) {
+        this.threshold = threshold;
+    }
+
+    public String getVmaptype() {
+        return vmaptype;
+    }
+
+    public void setVmaptype(String vmaptype) {
+        this.vmaptype = vmaptype;
+    }
+
+    public LayerGraphicWarehouseSupport getWarehouse() {
+        return warehouse;
+    }
+
+    public void setWarehouse(LayerGraphicWarehouseSupport warehouse) {
+        this.warehouse = warehouse;
+    }
+
+    public float getZero_eps() {
+        return zero_eps;
+    }
+
+    public void setZero_eps(float zero_eps) {
+        this.zero_eps = zero_eps;
+    }
+
     public static void usage() {
         System.out.println("Usage: java VMAP2Shape [args] <outfile.shp>");
         System.out.println("Arguments:");
         System.out.println("\t-props <path>             path to properties file");
         System.out.println("                            default: "
-                + propsFileName);
+                + DEF_PROPS_FILE_NAME);
         System.out.println("\t-prefix <identifier>      vmap properties prefix");
-        System.out.println("                            default: " + prefix);
+        System.out.println("                            default: " + DEF_PREFIX);
         System.out.println("\t-thin <eps> <thresh>      do thinning");
-        System.out.println("                            default eps=" + fan_eps
-                + " thresh=" + threshold);
+        System.out.println("                            default eps="
+                + DEF_FAN_EPS + " thresh=" + DEF_THRESHOLD);
         System.exit(1);
     }
 
@@ -406,19 +522,19 @@ public class VMAP2Shape {
 
         for (int i = 0; i < args.length - 1; i++) {
             if (args[i].equalsIgnoreCase("-props")) {
-                propsFileName = args[++i];
+                c.setPropsFileName(args[++i]);
             } else if (args[i].equalsIgnoreCase("-prefix")) {
-                prefix = args[++i];
+                c.setPrefix(args[++i]);
             } else if (args[i].equalsIgnoreCase("-thin")) {
-                doThinning = true;
-                fan_eps = Float.valueOf(args[++i]).floatValue();
-                threshold = Float.valueOf(args[++i]).floatValue();
+                c.setDoThinning(true);
+                c.setFan_eps(Float.valueOf(args[++i]).floatValue());
+                c.setThreshold(Float.valueOf(args[++i]).floatValue());
             } else {
                 usage();
             }
         }
 
-        c.setProperties(prefix, c.loadProperties());
-        c.writeShapeFile(args[args.length - 1], c.getRectangle());
+        c.writeShapeFile(args[args.length - 1]);
+
     }
 }
