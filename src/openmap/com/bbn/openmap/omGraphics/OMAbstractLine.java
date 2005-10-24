c4019f08a218f6e0f50a89624492f867d0be8f8b
dietrick <dietrick@f68c23cc-7f87-4fbc-acfa-d9760b90acc1>
2005-10-24 14:32:05 +0000

    Added arrow query methods per user request.
    git-svn-id: svn+ssh://src.dsl.bbn.com/svn/openmap/trunk@1521 f68c23cc-7f87-4fbc-acfa-d9760b90acc1

--- src/openmap/com/bbn/openmap/omGraphics/OMAbstractLine.java
+++ src/openmap/com/bbn/openmap/omGraphics/OMAbstractLine.java
@@ -2,7 +2,7 @@
  * Created on Jul 13, 2005
  * 
  * @author srosset
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 package com.bbn.openmap.omGraphics;
 
@@ -19,171 +19,195 @@ import java.awt.geom.GeneralPath;
  */
 public abstract class OMAbstractLine extends OMGraphic {
 
-  /**
-   * X coordinate arrays of the projected points.
-   */
-  protected int[][] xpoints = new int[0][0];
+    /**
+     * X coordinate arrays of the projected points.
+     */
+    protected int[][] xpoints = new int[0][0];
 
-  /**
-   * Y coordinate arrays of the projected points.
-   */
-  protected int[][] ypoints = new int[0][0];
+    /**
+     * Y coordinate arrays of the projected points.
+     */
+    protected int[][] ypoints = new int[0][0];
 
-  /**
-   * Number of segments to draw (used only for LINETYPE_GREATCIRCLE
-   * or LINETYPE_RHUMB lines).
-   */
-  protected int nsegs = -1;
-  
-  /**
-   * The awt object representing the arrowhead.
-   */
-  OMArrowHead arrowhead = null;
+    /**
+     * Number of segments to draw (used only for LINETYPE_GREATCIRCLE
+     * or LINETYPE_RHUMB lines).
+     */
+    protected int nsegs = -1;
 
-  /**
-   * Construct a default OMAbstractLine.
-   */
-  public OMAbstractLine() {
-    super();
-  }
+    /**
+     * The awt object representing the arrowhead.
+     */
+    OMArrowHead arrowhead = null;
 
-  /**
-   * Construct an OMAbstractLine. Standard simple constructor that the
-   * child OMAbstractLine usually call. All of the other parameters get
-   * set to their default values.
-   * 
-   * @param rType render type
-   * @param lType line type
-   * @param dcType declutter type
-   */
-  public OMAbstractLine(int rType, int lType, int dcType) {
-    super(rType, lType, dcType);
-  }
-  
-  /**
-   * Construct an OMAbstractLine. More complex constructor that lets you
-   * set the rest of the parameters.
-   * 
-   * @param rType render type
-   * @param lType line type
-   * @param dcType declutter type
-   * @param lc line color
-   * @param fc fill color
-   * @param sc select color
-   */
-  public OMAbstractLine(int rType, int lType, int dcType, Color lc, Color fc, Color sc) {
-    super(rType, lType, dcType, lc, fc, sc);
-  }
-  
-  /**
-   * Turn the ArrowHead on/off. The ArrowHead is placed on the
-   * finishing end.
-   * 
-   * @param value on/off
-   */
-  public void addArrowHead(boolean value) {
-      if (value) {
-          setArrowHead(new OMArrowHead(OMArrowHead.ARROWHEAD_DIRECTION_FORWARD, 100));
-      } else {
-          arrowhead = null;
-      }
-  }
-  
-  /**
-   * Set the OMArrowHead object on the OMAbstractLine.
-   * @param omah
-   */
-  public void setArrowHead(OMArrowHead omah) {
-      arrowhead = omah;
-  }
-  
-  public OMArrowHead getArrowHead() {
-      return arrowhead;
-  }
-  
-  public boolean hasArrowHead() {
-      return arrowhead != null;
-  }
+    /**
+     * Construct a default OMAbstractLine.
+     */
+    public OMAbstractLine() {
+        super();
+    }
 
-  /**
-   * Turn the ArrowHead on. The ArrowHead is placed on the finishing
-   * end (OMArrowHead.ARROWHEAD_DIRECTION_FORWARD), beginning end
-   * (OMArrowHead.ARROWHEAD_DIRECTION_BACKWARD), or both
-   * ends(OMArrowHead.ARROWHEAD_DIRECTION_BOTH).
-   * 
-   * @param directionType which way to point the arrow head.
-   */
-  public void addArrowHead(int directionType) {
-      setArrowHead(new OMArrowHead(directionType, 100));
-  }
+    /**
+     * Construct an OMAbstractLine. Standard simple constructor that
+     * the child OMAbstractLine usually call. All of the other
+     * parameters get set to their default values.
+     * 
+     * @param rType render type
+     * @param lType line type
+     * @param dcType declutter type
+     */
+    public OMAbstractLine(int rType, int lType, int dcType) {
+        super(rType, lType, dcType);
+    }
 
-  /**
-   * Turn the ArrowHead on. The ArrowHead is placed on the finishing
-   * end (OMArrowHead.ARROWHEAD_DIRECTION_FORWARD), beginning end
-   * (OMArrowHead.ARROWHEAD_DIRECTION_BACKWARD), or both
-   * ends(OMArrowHead.ARROWHEAD_DIRECTION_BOTH).
-   * 
-   * @param directionType which way to point the arrow head.
-   * @param location where on the line to put the arrow head - 0 for
-   *        the starting point, 100 for the end.
-   */
-  public void addArrowHead(int directionType, int location) {
-      setArrowHead(new OMArrowHead(directionType, location));
-  }
+    /**
+     * Construct an OMAbstractLine. More complex constructor that lets
+     * you set the rest of the parameters.
+     * 
+     * @param rType render type
+     * @param lType line type
+     * @param dcType declutter type
+     * @param lc line color
+     * @param fc fill color
+     * @param sc select color
+     */
+    public OMAbstractLine(int rType, int lType, int dcType, Color lc, Color fc,
+            Color sc) {
+        super(rType, lType, dcType, lc, fc, sc);
+    }
 
-  /**
-   * Turn the ArrowHead on. The ArrowHead is placed on the finishing
-   * end (OMArrowHead.ARROWHEAD_DIRECTION_FORWARD), beginning end
-   * (OMArrowHead.ARROWHEAD_DIRECTION_BACKWARD), or both
-   * ends(OMArrowHead.ARROWHEAD_DIRECTION_BOTH).
-   * 
-   * @param directionType which way to point the arrow head.
-   * @param location where on the line to put the arrow head - 0 for
-   *        the starting point, 100 for the end.
-   * @param tipWidth the width factor for the base of the arrowhead,
-   *        on one side of the line. (Default is 5)
-   * @param arrowLength the length factor of the arrowhead, from the
-   *        tip of the line to the base of the arrowhead. (Default
-   *        is 20)
-   */
-  public void addArrowHead(int directionType, int location, int tipWidth,
-                           int arrowLength) {
-      setArrowHead(new OMArrowHead(directionType, location, tipWidth, arrowLength));
-  }
+    /**
+     * Turn the ArrowHead on/off. The ArrowHead is placed on the
+     * finishing end.
+     * 
+     * @param value on/off
+     */
+    public void addArrowHead(boolean value) {
+        if (value) {
+            setArrowHead(new OMArrowHead(OMArrowHead.ARROWHEAD_DIRECTION_FORWARD, 100));
+        } else {
+            arrowhead = null;
+        }
+    }
 
-  /**
-   * Arrowhead function, to find out the wing tip width.
-   */
-  public int getWingTip() {
-      if (arrowhead != null) {
-          return arrowhead.getWingTip();
-      }
-      return 0;
-  }
+    /**
+     * Set the OMArrowHead object on the OMAbstractLine.
+     * 
+     * @param omah
+     */
+    public void setArrowHead(OMArrowHead omah) {
+        arrowhead = omah;
+    }
 
-  /**
-   * Arrowhead function, to find out the arrowhead length.
-   */
-  public int getWingLength() {
-      if (arrowhead != null) {
-          return arrowhead.getWingLength();
-      }
-      return 0;
-  }
-  
-  /**
-   * This is a method that you can extend to create the GeneralPath
-   * for the arrowheads, if you want a different way of doing it. By
-   * default, it calls OMArrowHead.createArrowHeads(), using the
-   * different arrowhead variables set in the OMLine.
-   * @deprecated Create an OMArrowHead and set it on the OMAbstractLine instead.
-   */
-  public GeneralPath createArrowHeads() {
-      return OMArrowHead.createArrowHeads(OMArrowHead.ARROWHEAD_DIRECTION_FORWARD,
-              100,
-              this,
-              OMArrowHead.DEFAULT_WINGTIP,
-              OMArrowHead.DEFAULT_WINGLENGTH);
-  }
-  
+    public OMArrowHead getArrowHead() {
+        return arrowhead;
+    }
+
+    public boolean hasArrowHead() {
+        return arrowhead != null;
+    }
+
+    /**
+     * Turn the ArrowHead on. The ArrowHead is placed on the finishing
+     * end (OMArrowHead.ARROWHEAD_DIRECTION_FORWARD), beginning end
+     * (OMArrowHead.ARROWHEAD_DIRECTION_BACKWARD), or both
+     * ends(OMArrowHead.ARROWHEAD_DIRECTION_BOTH).
+     * 
+     * @param directionType which way to point the arrow head.
+     */
+    public void addArrowHead(int directionType) {
+        setArrowHead(new OMArrowHead(directionType, 100));
+    }
+
+    /**
+     * Turn the ArrowHead on. The ArrowHead is placed on the finishing
+     * end (OMArrowHead.ARROWHEAD_DIRECTION_FORWARD), beginning end
+     * (OMArrowHead.ARROWHEAD_DIRECTION_BACKWARD), or both
+     * ends(OMArrowHead.ARROWHEAD_DIRECTION_BOTH).
+     * 
+     * @param directionType which way to point the arrow head.
+     * @param location where on the line to put the arrow head - 0 for
+     *        the starting point, 100 for the end.
+     */
+    public void addArrowHead(int directionType, int location) {
+        setArrowHead(new OMArrowHead(directionType, location));
+    }
+
+    /**
+     * Turn the ArrowHead on. The ArrowHead is placed on the finishing
+     * end (OMArrowHead.ARROWHEAD_DIRECTION_FORWARD), beginning end
+     * (OMArrowHead.ARROWHEAD_DIRECTION_BACKWARD), or both
+     * ends(OMArrowHead.ARROWHEAD_DIRECTION_BOTH).
+     * 
+     * @param directionType which way to point the arrow head.
+     * @param location where on the line to put the arrow head - 0 for
+     *        the starting point, 100 for the end.
+     * @param tipWidth the width factor for the base of the arrowhead,
+     *        on one side of the line. (Default is 5)
+     * @param arrowLength the length factor of the arrowhead, from the
+     *        tip of the line to the base of the arrowhead. (Default
+     *        is 20)
+     */
+    public void addArrowHead(int directionType, int location, int tipWidth,
+                             int arrowLength) {
+        setArrowHead(new OMArrowHead(directionType, location, tipWidth, arrowLength));
+    }
+
+    /**
+     * Arrowhead function, to find out the wing tip width.
+     */
+    public int getWingTip() {
+        if (arrowhead != null) {
+            return arrowhead.getWingTip();
+        }
+        return 0;
+    }
+
+    /**
+     * Arrowhead function, to find out the arrowhead length.
+     */
+    public int getWingLength() {
+        if (arrowhead != null) {
+            return arrowhead.getWingLength();
+        }
+        return 0;
+    }
+
+    /**
+     * Arrowhead function, to find out the arrowhead location.
+     */
+    public int getArrowLocation() {
+        if (arrowhead != null) {
+            return arrowhead.getLocation();
+        }
+        return 0;
+    }
+
+    /**
+     * Arrowhead function, to find out the arrowhead direction.
+     */
+    public int getArrowDirectionType() {
+        if (arrowhead != null) {
+            return arrowhead.getArrowDirectionType();
+        }
+        return 0;
+    }
+
+    /**
+     * This is a method that you can extend to create the GeneralPath
+     * for the arrowheads, if you want a different way of doing it. By
+     * default, it calls OMArrowHead.createArrowHeads(), using the
+     * different arrowhead variables set in the OMLine.
+     * 
+     * @deprecated Create an OMArrowHead and set it on the
+     *             OMAbstractLine instead.
+     */
+    public GeneralPath createArrowHeads() {
+        return OMArrowHead.createArrowHeads(OMArrowHead.ARROWHEAD_DIRECTION_FORWARD,
+                100,
+                this,
+                OMArrowHead.DEFAULT_WINGTIP,
+                OMArrowHead.DEFAULT_WINGLENGTH);
+    }
+
 }
