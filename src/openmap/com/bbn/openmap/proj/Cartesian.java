f200e6d117ff99b650b63a6aa5c20e5d3a52a014
dietrick <dietrick@f68c23cc-7f87-4fbc-acfa-d9760b90acc1>
2006-02-24 20:43:27 +0000

    Update Cartesian projection to have top, bottom, left and right coordinate limits on what the projection will display.  The limits can have an affect on scale if limits on opposing boundaries are set.  The limits can be set programmatically or via the CartesianLoader properties.
    git-svn-id: svn+ssh://src.dsl.bbn.com/svn/openmap/trunk@1606 f68c23cc-7f87-4fbc-acfa-d9760b90acc1

--- src/openmap/com/bbn/openmap/proj/Cartesian.java
+++ src/openmap/com/bbn/openmap/proj/Cartesian.java
@@ -14,8 +14,8 @@
 // 
 // $Source: /cvs/distapps/openmap/src/openmap/com/bbn/openmap/proj/Cartesian.java,v $
 // $RCSfile: Cartesian.java,v $
-// $Revision: 1.2 $
-// $Date: 2006/02/16 16:22:46 $
+// $Revision: 1.3 $
+// $Date: 2006/02/24 20:43:27 $
 // $Author: dietrick $
 // 
 // **********************************************************************
@@ -37,8 +37,37 @@ public class Cartesian extends Proj implements Projection, java.io.Serializable 
      */
     public final static transient String CartesianName = "Cartesian";
 
-    protected Point2D ulLimit;
-    protected Point2D lrLimit;
+    /**
+     * The coordinate limit of the left side of the projection. If the left side
+     * of the map projection would show coordinates more left than this value,
+     * the center of the map will be changed so that this value is on the edge.
+     */
+    protected double leftLimit;
+    /**
+     * The coordinate limit of the right side of the projection. If the right
+     * side of the map projection would show coordinates more right than this
+     * value, the center of the map will be changed so that this value is on the
+     * edge.
+     */
+    protected double rightLimit;
+    /**
+     * The coordinate limit of the top side of the projection. If the top side
+     * of the map projection would show coordinates higher than this value, the
+     * center of the map will be changed so that this value is on the edge.
+     */
+    protected double topLimit;
+    /**
+     * The coordinate limit of the bottom side of the projection. If the bottom
+     * side of the map projection would show coordinates lower than this value,
+     * the center of the map will be changed so that this value is on the edge.
+     */
+    protected double bottomLimit;
+    /**
+     * A point that can be used for force the projection against the limits. Is
+     * only used if the limits are set to be something other than infinity.
+     */
+    protected Point2D limitAnchorPoint;
+
     protected double scaleFactor;
 
     protected transient double hWidth;
@@ -65,6 +94,14 @@ public class Cartesian extends Proj implements Projection, java.io.Serializable 
 
     public void init() {
         scaleFactor = 100000000;
+
+        // Limits are in coordinate space, dictating how much coordinate space
+        // can be viewable. The center point and scale should be adjusted as
+        // appropriate (during computeParameters).
+        leftLimit = Double.NEGATIVE_INFINITY;
+        rightLimit = Double.POSITIVE_INFINITY;
+        topLimit = Double.POSITIVE_INFINITY;
+        bottomLimit = Double.NEGATIVE_INFINITY;
     }
 
     protected void computeParameters() {
@@ -72,11 +109,150 @@ public class Cartesian extends Proj implements Projection, java.io.Serializable 
         hHeight = height / 2.0;
         SFScale = scaleFactor / scale;
 
+        checkLimits();
+
         transform1 = AffineTransform.getTranslateInstance(-centerX, -centerY);
         transform2 = AffineTransform.getScaleInstance(SFScale, -SFScale);
         // transform3 = AffineTransform.getScaleInstance(1 / SFScale,
         // -1 / SFScale);
         transform4 = AffineTransform.getTranslateInstance(hWidth, hHeight);
+    }
+
+    /**
+     * The method you want to call. Checks if there are limits set, and will
+     * call setLimits(checkScale) properly.
+     */
+    protected void checkLimits() {
+        if (leftLimit != Double.NEGATIVE_INFINITY
+                || rightLimit != Double.POSITIVE_INFINITY
+                || topLimit != Double.POSITIVE_INFINITY
+                || bottomLimit != Double.NEGATIVE_INFINITY) {
+            checkLimits(true);
+        }
+    }
+
+    /**
+     * Should only be called if you've checked there are limits set on the
+     * projection.
+     * 
+     * @param checkScale true the first time through, if the scale changes this
+     *        method calls itself with false to prevent a loop.
+     */
+    protected void checkLimits(boolean checkScale) {
+
+        if (limitAnchorPoint != null) {
+            centerX = limitAnchorPoint.getX();
+            centerY = limitAnchorPoint.getY();
+        }
+
+        // Check limits.
+        Point2D p2 = checkUpperLimits();
+        if (p2 != null) {
+            centerX = p2.getX();
+            centerY = p2.getY();
+        }
+
+        Point2D p1 = checkLowerLimits();
+        if (p1 != null) {
+            centerX = p1.getX();
+            centerY = p1.getY();
+        }
+
+        if (checkScale && p1 != null && p2 != null) {
+            double newScale = checkScaleAgainstLimits();
+            if (newScale != scale) {
+                scale = newScale;
+                SFScale = scaleFactor / scale;
+                checkLimits(false);
+            }
+        }
+    }
+
+    /**
+     * Checks the lower limits and returns new center coordinates to keep the
+     * limits at the edge if necessary.
+     */
+    protected Point2D checkLowerLimits() {
+        Point2D p1 = new Point2D.Double();
+        Point2D p2 = null;
+        double moveX = 0;
+        double moveY = 0;
+        inverse(0, height, p1);// LL corner
+        if (p1.getX() < leftLimit) {
+            p2 = forward(centerY, leftLimit, p2);
+            moveX = p2.getX();
+        }
+
+        if (p1.getY() < bottomLimit) {
+            p2 = forward(bottomLimit, centerX, p2);
+            moveY = height - p2.getY();
+        }
+
+        if (p2 != null) {
+            inverse(hWidth + moveX, hHeight - moveY, p2);
+        }
+
+        return p2;
+    }
+
+    /**
+     * Checks the upper limits and returns new center coordinates to keep the
+     * limits at the edge if necessary.
+     */
+    protected Point2D checkUpperLimits() {
+        Point2D p1 = new Point2D.Double();
+        Point2D p2 = null;
+        double moveX = 0;
+        double moveY = 0;
+        inverse(width, 0, p1);// UR corner
+        if (p1.getX() > rightLimit) {
+            p2 = forward(centerY, rightLimit, p2);
+            moveX = width - p2.getX();
+        }
+
+        if (p1.getY() > topLimit) {
+            p2 = forward(topLimit, centerX, p2);
+            moveY = p2.getY();
+        }
+
+        if (p2 != null) {
+            inverse(hWidth - moveX, hHeight + moveY, p2);
+        }
+
+        return p2;
+    }
+
+    /**
+     * Checks the corner values against the limits and returns the right scale
+     * to keep limits at the edge if necessary.
+     */
+    protected double checkScaleAgainstLimits() {
+        Point2D p1 = getUpperLeft();
+        Point2D p2 = getLowerRight();
+
+        double x = p1.getX();
+        double y = p1.getY();
+        if (topLimit != Double.POSITIVE_INFINITY) {
+            y = topLimit;
+        }
+
+        if (leftLimit != Double.NEGATIVE_INFINITY) {
+            x = leftLimit;
+        }
+        p1.setLocation(x, y);
+
+        x = p2.getX();
+        y = p2.getY();
+        if (bottomLimit != Double.NEGATIVE_INFINITY) {
+            y = bottomLimit;
+        }
+        if (rightLimit != Double.POSITIVE_INFINITY) {
+            x = rightLimit;
+        }
+
+        p2.setLocation(x, y);
+        // second p1, p2 meaningless
+        return getScale(p1, p2, p1, p2);
     }
 
     /**
@@ -224,6 +400,61 @@ public class Cartesian extends Proj implements Projection, java.io.Serializable 
 
     public boolean isPlotable(double lat, double lon) {
         return true;
+    }
+
+    public double getBottomLimit() {
+        return bottomLimit;
+    }
+
+    public void setBottomLimit(double bottomLimit) {
+        this.bottomLimit = bottomLimit;
+        computeParameters();
+    }
+
+    public double getLeftLimit() {
+        return leftLimit;
+    }
+
+    public void setLeftLimit(double leftLimit) {
+        this.leftLimit = leftLimit;
+        computeParameters();
+    }
+
+    public Point2D getLimitAnchorPoint() {
+        return limitAnchorPoint;
+    }
+
+    public void setLimitAnchorPoint(Point2D limitAnchorPoint) {
+        this.limitAnchorPoint = limitAnchorPoint;
+        computeParameters();
+    }
+
+    public double getRightLimit() {
+        return rightLimit;
+    }
+
+    public void setRightLimit(double rightLimit) {
+        this.rightLimit = rightLimit;
+        computeParameters();
+    }
+
+    public double getTopLimit() {
+        return topLimit;
+    }
+
+    public void setTopLimit(double topLimit) {
+        this.topLimit = topLimit;
+        computeParameters();
+    }
+
+    public void setLimits(double top, double bottom, double left, double right,
+                          Point2D anchor) {
+        this.topLimit = top;
+        this.bottomLimit = bottom;
+        this.leftLimit = left;
+        this.rightLimit = right;
+        this.limitAnchorPoint = anchor;
+        computeParameters();
     }
 
 }
--- src/openmap/com/bbn/openmap/proj/CartesianLoader.java
+++ src/openmap/com/bbn/openmap/proj/CartesianLoader.java
@@ -14,8 +14,8 @@
 // 
 // $Source: /cvs/distapps/openmap/src/openmap/com/bbn/openmap/proj/CartesianLoader.java,v $
 // $RCSfile: CartesianLoader.java,v $
-// $Revision: 1.1 $
-// $Date: 2005/12/09 21:09:01 $
+// $Revision: 1.2 $
+// $Date: 2006/02/24 20:43:27 $
 // $Author: dietrick $
 // 
 // **********************************************************************
@@ -29,13 +29,63 @@ import com.bbn.openmap.util.Debug;
 import com.bbn.openmap.util.PropUtils;
 
 /**
- * ProjectionLoader to add the Cartesian projection to an OpenMap
- * application.
+ * ProjectionLoader to add the Cartesian projection to an OpenMap application.
+ * There are some properties you can set for the Cartesian projection, namely
+ * limits on where it can pan. If you don't set the limits, the projection will
+ * just keep going. You can use the anchorX and anchorY settings to hold the map
+ * against some of the limits.
+ * 
+ * <pre>
+ *  topLimit=top coordinate limit
+ *  bottomLimit=bottom coordinate limit
+ *  leftLimit=left side coordinate limit
+ *  rightLimit=right side coordinate limit
+ *  anchorX=horizontal coordinate to set on projection before checking limits
+ *  anchorY=vertical coordinate to set on projection before checking limits
+ * </pre>
  * 
  * @see BasicProjectionLoader
  */
 public class CartesianLoader extends BasicProjectionLoader implements
         ProjectionLoader {
+
+    public final static String TopLimitProperty = "topLimit";
+    public final static String BottomLimitProperty = "bottomLimit";
+    public final static String RightLimitProperty = "rightLimit";
+    public final static String LeftLimitProperty = "leftLimit";
+    public final static String AnchorXProperty = "anchorX";
+    public final static String AnchorYProperty = "anchorY";
+
+    /**
+     * The coordinate limit of the left side of the projection. If the left side
+     * of the map projection would show coordinates more left than this value,
+     * the center of the map will be changed so that this value is on the edge.
+     */
+    protected double leftLimit = Double.NEGATIVE_INFINITY;
+    /**
+     * The coordinate limit of the right side of the projection. If the right
+     * side of the map projection would show coordinates more right than this
+     * value, the center of the map will be changed so that this value is on the
+     * edge.
+     */
+    protected double rightLimit = Double.POSITIVE_INFINITY;
+    /**
+     * The coordinate limit of the top side of the projection. If the top side
+     * of the map projection would show coordinates higher than this value, the
+     * center of the map will be changed so that this value is on the edge.
+     */
+    protected double topLimit = Double.POSITIVE_INFINITY;
+    /**
+     * The coordinate limit of the bottom side of the projection. If the bottom
+     * side of the map projection would show coordinates lower than this value,
+     * the center of the map will be changed so that this value is on the edge.
+     */
+    protected double bottomLimit = Double.NEGATIVE_INFINITY;
+    /**
+     * A point that can be used for force the projection against the limits. Is
+     * only used if the limits are set to be something other than infinity.
+     */
+    protected Point2D limitAnchorPoint;
 
     public CartesianLoader() {
         super(Cartesian.class,
@@ -61,7 +111,16 @@ public class CartesianLoader extends BasicProjectionLoader implements
             int width = PropUtils.intFromProperties(props,
                     ProjectionFactory.WIDTH,
                     100);
-            return new Cartesian(center, scale, width, height);
+
+            Cartesian proj = new Cartesian(center, scale, width, height);
+
+            proj.setLimits(topLimit,
+                    bottomLimit,
+                    leftLimit,
+                    rightLimit,
+                    limitAnchorPoint);
+
+            return proj;
 
         } catch (Exception e) {
             if (Debug.debugging("proj")) {
@@ -71,6 +130,159 @@ public class CartesianLoader extends BasicProjectionLoader implements
         }
 
         throw new ProjectionException("CartesianLoader: problem creating Cartesian projection");
+    }
+
+    public void setProperties(String prefix, Properties props) {
+        super.setProperties(prefix, props);
+        prefix = PropUtils.getScopedPropertyPrefix(prefix);
+
+        leftLimit = PropUtils.doubleFromProperties(props, prefix
+                + LeftLimitProperty, Double.NEGATIVE_INFINITY);
+        rightLimit = PropUtils.doubleFromProperties(props, prefix
+                + RightLimitProperty, Double.POSITIVE_INFINITY);
+        topLimit = PropUtils.doubleFromProperties(props, prefix
+                + TopLimitProperty, Double.POSITIVE_INFINITY);
+        bottomLimit = PropUtils.doubleFromProperties(props, prefix
+                + BottomLimitProperty, Double.NEGATIVE_INFINITY);
+
+        double x = PropUtils.doubleFromProperties(props, prefix
+                + AnchorXProperty, Double.POSITIVE_INFINITY);
+        double y = PropUtils.doubleFromProperties(props, prefix
+                + AnchorYProperty, Double.POSITIVE_INFINITY);
+
+        if (x != Double.POSITIVE_INFINITY && y != Double.POSITIVE_INFINITY) {
+            limitAnchorPoint = new Point2D.Double(x, y);
+        }
+
+    }
+
+    public Properties getProperties(Properties props) {
+        props = super.getProperties(props);
+        String prefix = PropUtils.getScopedPropertyPrefix(this);
+        String x = "";
+        String y = "";
+        String top = "";
+        String bottom = "";
+        String left = "";
+        String right = "";
+        if (leftLimit != Double.NEGATIVE_INFINITY) {
+            left = Double.toString(leftLimit);
+        }
+        if (topLimit != Double.POSITIVE_INFINITY) {
+            top = Double.toString(topLimit);
+        }
+        if (rightLimit != Double.POSITIVE_INFINITY) {
+            right = Double.toString(rightLimit);
+        }
+        if (bottomLimit != Double.NEGATIVE_INFINITY) {
+            bottom = Double.toString(bottomLimit);
+        }
+        props.put(prefix + TopLimitProperty, top);
+        props.put(prefix + BottomLimitProperty, bottom);
+        props.put(prefix + RightLimitProperty, right);
+        props.put(prefix + LeftLimitProperty, left);
+        if (limitAnchorPoint != null) {
+            x = Double.toString(limitAnchorPoint.getX());
+            y = Double.toString(limitAnchorPoint.getY());
+        }
+        props.put(prefix + AnchorXProperty, x);
+        props.put(prefix + AnchorYProperty, y);
+        return props;
+    }
+
+    public Properties getPropertyInfo(Properties props) {
+        props = super.getPropertyInfo(props);
+
+        PropUtils.setI18NPropertyInfo(i18n,
+                props,
+                CartesianLoader.class,
+                LeftLimitProperty,
+                "Left Limit",
+                "Coordinate limit for the left side of the map.",
+                null);
+        PropUtils.setI18NPropertyInfo(i18n,
+                props,
+                CartesianLoader.class,
+                RightLimitProperty,
+                "Right Limit",
+                "Coordinate limit for the right side of the map.",
+                null);
+        PropUtils.setI18NPropertyInfo(i18n,
+                props,
+                CartesianLoader.class,
+                TopLimitProperty,
+                "Top Limit",
+                "Coordinate limit for the top of the map.",
+                null);
+        PropUtils.setI18NPropertyInfo(i18n,
+                props,
+                CartesianLoader.class,
+                BottomLimitProperty,
+                "Bottom Limit",
+                "Coordinate limit for the bottom of the map.",
+                null);
+        PropUtils.setI18NPropertyInfo(i18n,
+                props,
+                CartesianLoader.class,
+                AnchorXProperty,
+                "Anchor X",
+                "Horizontal Coordinate for anchor point, used to hold projection against limits.",
+                null);
+        PropUtils.setI18NPropertyInfo(i18n,
+                props,
+                CartesianLoader.class,
+                AnchorYProperty,
+                "Anchor Y",
+                "Horizontal Coordinate for anchor point, used to hold projection against limits.",
+                null);
+
+        props.put(initPropertiesProperty, PrettyNameProperty + " "
+                + DescriptionProperty + " " + TopLimitProperty + " "
+                + BottomLimitProperty + " " + RightLimitProperty + " "
+                + LeftLimitProperty + " " + AnchorXProperty + " "
+                + AnchorYProperty);
+
+        return props;
+    }
+
+    public double getBottomLimit() {
+        return bottomLimit;
+    }
+
+    public void setBottomLimit(double bottomLimit) {
+        this.bottomLimit = bottomLimit;
+    }
+
+    public double getLeftLimit() {
+        return leftLimit;
+    }
+
+    public void setLeftLimit(double leftLimit) {
+        this.leftLimit = leftLimit;
+    }
+
+    public Point2D getLimitAnchorPoint() {
+        return limitAnchorPoint;
+    }
+
+    public void setLimitAnchorPoint(Point2D limitAnchorPoint) {
+        this.limitAnchorPoint = limitAnchorPoint;
+    }
+
+    public double getRightLimit() {
+        return rightLimit;
+    }
+
+    public void setRightLimit(double rightLimit) {
+        this.rightLimit = rightLimit;
+    }
+
+    public double getTopLimit() {
+        return topLimit;
+    }
+
+    public void setTopLimit(double topLimit) {
+        this.topLimit = topLimit;
     }
 
 }
