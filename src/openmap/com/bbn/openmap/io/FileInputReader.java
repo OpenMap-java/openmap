e81d74b55290f5426f35b127fd1762e211170aac
dietrick <dietrick@f68c23cc-7f87-4fbc-acfa-d9760b90acc1>
2008-10-24 21:29:40 +0000

    Added Benstein's updates to FileInputReader that save the file name in getName() vs the absolute path.  That was messing up the MakeToc class, and no other classes seemed to care.  Added a getAbsolutePath method to allow FIR to be able to reopen files.
    git-svn-id: svn+ssh://src.dsl.bbn.com/svn/openmap/trunk@1921 f68c23cc-7f87-4fbc-acfa-d9760b90acc1

--- src/openmap/com/bbn/openmap/io/FileInputReader.java
+++ src/openmap/com/bbn/openmap/io/FileInputReader.java
@@ -14,12 +14,11 @@
 // 
 // $Source: /cvs/distapps/openmap/src/openmap/com/bbn/openmap/io/FileInputReader.java,v $
 // $RCSfile: FileInputReader.java,v $
-// $Revision: 1.4 $
-// $Date: 2007/06/21 21:39:02 $
+// $Revision: 1.5 $
+// $Date: 2008/10/24 21:28:21 $
 // $Author: dietrick $
 // 
 // **********************************************************************
-
 
 package com.bbn.openmap.io;
 
@@ -27,54 +26,65 @@ import com.bbn.openmap.util.Debug;
 import java.io.*;
 
 /**
- * This class wraps a java.io.RandomAccessFile to allow us to choose
- * the byte-order of the underlying file.  It has a user-settable
- * byte-ordering, which is then used to properly implement the various
- * multibyte reading members.  Used for reading local files.
- *
- * @see java.io.RandomAccessFile 
- * @see com.bbn.openmap.io.InputReader 
+ * This class wraps a java.io.RandomAccessFile to allow us to choose the
+ * byte-order of the underlying file. It has a user-settable byte-ordering,
+ * which is then used to properly implement the various multibyte reading
+ * members. Used for reading local files.
+ * 
+ * @see java.io.RandomAccessFile
+ * @see com.bbn.openmap.io.InputReader
  * @see com.bbn.openmap.io.BinaryFile
  */
 public class FileInputReader implements InputReader {
     /** The underlying file input */
     protected RandomAccessFile inputFile = null;
+    /**
+     * Needed for input reader interface.
+     */
     protected String name = null;
+    /**
+     * Needed for reopening files that have been closed.
+     */
+    protected String absolutePath = null;
 
     /**
-     * Constructs a new BinaryFile with the specified file as the
-     * input.  The default byte-order is LSB first.  Reads start at
-     * the first byte of the file.
-     *
+     * Constructs a new BinaryFile with the specified file as the input. The
+     * default byte-order is LSB first. Reads start at the first byte of the
+     * file.
+     * 
      * @param f the file to be opened for reading
      * @exception IOException pass-through errors from opening a
-     * RandomAccessFile with f
-     * @see java.io.RandomAccessFile 
+     *            RandomAccessFile with f
+     * @see java.io.RandomAccessFile
      */
     public FileInputReader(File f) throws IOException {
         if (Debug.debugging("binaryfile")) {
             Debug.output("FileInputReader created from " + f.getAbsolutePath());
         }
-        name = f.getAbsolutePath();
+        name = f.getName();
+        absolutePath = f.getAbsolutePath();
         inputFile = init(f);
     }
 
     /**
-     * Constructs a new BinaryFile with the specified file as the
-     * input.  The default byte-order is LSB first.  Reads start at
-     * the first byte of the file.
-     *
+     * Constructs a new BinaryFile with the specified file as the input. The
+     * default byte-order is LSB first. Reads start at the first byte of the
+     * file.
+     * 
      * @param f the path to the file to be opened for reading.
      * @exception IOException pass-through errors from opening a
-     * RandomAccessFile with f
-     * @see java.io.RandomAccessFile 
+     *            RandomAccessFile with f
+     * @see java.io.RandomAccessFile
      */
     public FileInputReader(String f) throws IOException {
         if (Debug.debugging("binaryfile")) {
             Debug.output("FileInputReader created from " + f);
         }
-        name = f;
-        inputFile = init(new File(f));
+
+        File file = new File(f);
+        name = file.getName();
+        absolutePath = file.getAbsolutePath();
+        inputFile = init(file);
     }
 
     /**
@@ -83,12 +93,19 @@ public class FileInputReader implements InputReader {
     public String getName() {
         return name;
     }
+    
+    /**
+     * @return the absolute path of the file.
+     */
+    public String getAbsolutePath() {
+        return absolutePath;
+    }
 
     /**
-     * Initialize the underlying RandomAccessFile.  If it's found, but
-     * there are too many files open, it calls
-     * BinaryFile.closeClosable to try to get an open file pointer
-     * from the system, and then tries again.
+     * Initialize the underlying RandomAccessFile. If it's found, but there are
+     * too many files open, it calls BinaryFile.closeClosable to try to get an
+     * open file pointer from the system, and then tries again.
+     * 
      * @param f a java.io.File
      * @throws IOException
      */
@@ -100,7 +117,7 @@ public class FileInputReader implements InputReader {
             if (i instanceof FileNotFoundException) {
                 throw i;
             }
-            
+
             if (f.canRead()) {
                 BinaryFile.closeClosable();
                 inputFile = new RandomAccessFile(f, "r");
@@ -112,38 +129,38 @@ public class FileInputReader implements InputReader {
     }
 
     /**
-     * Get the RandomAccessFile, for quering purposes only.  Don't use
-     * it to get data!
+     * Get the RandomAccessFile, for quering purposes only. Don't use it to get
+     * data!
      */
     public RandomAccessFile getInputFile() {
         return inputFile;
     }
 
     public RandomAccessFile checkInputFile() throws IOException {
-        if (inputFile == null && name != null) {
-            inputFile = init(new File(name));
+        if (inputFile == null && absolutePath != null) {
+            inputFile = init(new File(absolutePath));
         }
         return inputFile;
     }
-    
+
     /**
      * Skip over n bytes in the input file
-     *
+     * 
      * @param n the number of bytes to skip
-     * @return the actual number of bytes skipped.  annoying, isn't it?
-     * @exception IOException Any IO errors that occur in skipping bytes
-     * in the underlying file
+     * @return the actual number of bytes skipped. annoying, isn't it?
+     * @exception IOException Any IO errors that occur in skipping bytes in the
+     *            underlying file
      */
     public long skipBytes(long n) throws IOException {
-        return checkInputFile().skipBytes((int)n);
+        return checkInputFile().skipBytes((int) n);
     }
 
     /**
      * Get the index of the next character to be read
-     *
+     * 
      * @return the index
-     * @exception IOException Any IO errors that occur in accessing
-     * the underlying file
+     * @exception IOException Any IO errors that occur in accessing the
+     *            underlying file
      */
     public long getFilePointer() throws IOException {
         return checkInputFile().getFilePointer();
@@ -151,21 +168,21 @@ public class FileInputReader implements InputReader {
 
     /**
      * Set the index of the next character to be read.
-     *
+     * 
      * @param pos the position to seek to.
-     * @exception IOException Any IO Errors that occur in seeking the
-     * underlying file.
+     * @exception IOException Any IO Errors that occur in seeking the underlying
+     *            file.
      */
     public void seek(long pos) throws IOException {
         checkInputFile().seek(pos);
     }
 
     /**
-     * Local files only.  Retrieve the length of the file being accessed.
-     *
+     * Local files only. Retrieve the length of the file being accessed.
+     * 
      * @return the length of the file (counted in bytes)
      * @exception IOException Any IO errors that occur in accessing the
-     * underlying file.
+     *            underlying file.
      */
     public long length() throws IOException {
         return checkInputFile().length();
@@ -173,17 +190,17 @@ public class FileInputReader implements InputReader {
 
     /**
      * Return how many bytes left to be read in the file.
-     *
+     * 
      * @return the number of bytes remaining to be read (counted in bytes)
      * @exception IOException Any IO errors encountered in accessing the file
      */
     public long available() throws IOException {
-        return(length() - getFilePointer());
+        return (length() - getFilePointer());
     }
 
-    /** 
+    /**
      * Closes the underlying file
-     *
+     * 
      * @exception IOException Any IO errors encountered in accessing the file
      */
     public void close() throws IOException {
@@ -191,7 +208,8 @@ public class FileInputReader implements InputReader {
             Debug.output("FileInputReader.close()");
         }
         try {
-            if (inputFile != null) inputFile.close();
+            if (inputFile != null)
+                inputFile.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -200,8 +218,8 @@ public class FileInputReader implements InputReader {
 
     /**
      * Read from the file.
-     *
-     * @return one byte from the file.  -1 for EOF
+     * 
+     * @return one byte from the file. -1 for EOF
      * @exception IOException Any IO errors encountered in reading from the file
      */
     public int read() throws IOException {
@@ -210,7 +228,7 @@ public class FileInputReader implements InputReader {
 
     /**
      * Read from the file
-     *
+     * 
      * @param b The byte array to read into
      * @param off the first array position to read into
      * @param len the number of bytes to read
@@ -221,11 +239,11 @@ public class FileInputReader implements InputReader {
         return checkInputFile().read(b, off, len);
     }
 
-    /** 
+    /**
      * Read from the file.
-     *
-     * @param b the byte array to read into.  Equivelent to 
-     * <code>read(b, 0, b.length)</code>
+     * 
+     * @param b the byte array to read into. Equivelent to
+     *        <code>read(b, 0, b.length)</code>
      * @return the number of bytes read
      * @exception IOException Any IO errors encountered in reading from the file
      * @see java.io.RandomAccessFile#read(byte[])
@@ -234,20 +252,20 @@ public class FileInputReader implements InputReader {
         return checkInputFile().read(b);
     }
 
-    /** 
+    /**
      * Read from the file.
-     *
+     * 
      * @param howmany the number of bytes to read
      * @param allowless if we can return fewer bytes than requested
      * @return the array of bytes read.
      * @exception FormatException Any IO Exceptions, plus an end-of-file
-     * encountered after reading some, but now enough, bytes when allowless
-     * was <code>false</code>
-     * @exception EOFException Encountered an end-of-file while allowless 
-     * was <code>false</code>, but NO bytes had been read.
+     *            encountered after reading some, but now enough, bytes when
+     *            allowless was <code>false</code>
+     * @exception EOFException Encountered an end-of-file while allowless was
+     *            <code>false</code>, but NO bytes had been read.
      */
-    public byte[] readBytes(int howmany, boolean allowless) 
-        throws EOFException, FormatException {
+    public byte[] readBytes(int howmany, boolean allowless)
+            throws EOFException, FormatException {
         byte foo[] = new byte[howmany];
         int gotsofar = 0;
         int err = 0;
@@ -258,12 +276,13 @@ public class FileInputReader implements InputReader {
 
                 if (err == -1) {
                     if (allowless) {
-                        //return a smaller array, so the caller can tell how much
-                        //they really got
+                        // return a smaller array, so the caller can tell how
+                        // much
+                        // they really got
                         byte retval[] = new byte[gotsofar];
                         System.arraycopy(foo, 0, retval, 0, gotsofar);
                         return retval;
-                    } else { //some kind of failure...
+                    } else { // some kind of failure...
                         if (gotsofar > 0) {
                             throw new FormatException("EOF while reading data");
                         } else {
@@ -274,7 +293,8 @@ public class FileInputReader implements InputReader {
                 gotsofar += err;
             }
         } catch (IOException i) {
-            throw new FormatException("FileInputReader: readBytes IOException: " + i.getMessage());
+            throw new FormatException("FileInputReader: readBytes IOException: "
+                    + i.getMessage());
         }
         return foo;
     }
