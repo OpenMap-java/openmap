2711331f99423d33ad05dccf04945ff94bf0f4ad
dietrick <dietrick@f68c23cc-7f87-4fbc-acfa-d9760b90acc1>
2009-03-05 03:00:29 +0000

    Updated scripts to assume they are being run relative to top level directory instead of bin directory.  New build.xml packaging moves them up.
    git-svn-id: svn+ssh://src.dsl.bbn.com/svn/openmap/trunk@1946 f68c23cc-7f87-4fbc-acfa-d9760b90acc1

--- bin/openmap.bat
+++ bin/openmap.bat
@@ -10,11 +10,11 @@ set JAVABIN=java.exe
 rem OpenMap top-level directory
 rem THIS POINTS TO THE DIRECTORY ABOVE "BIN". CHANGE THIS IF YOU MOVE THE
 rem BATCH FILE.
-set OPENMAP_HOME=..
+set OPENMAP_HOME=.
 
 rem CLASSPATH points to toplevel OpenMap directory and share
 rem subdirectory.  Add other jar files as available.
 set CLASSPATH=%OPENMAP_HOME%;%OPENMAP_HOME%\classes\openmap;%OPENMAP_HOME%\share;%OPENMAP_HOME%\lib\milStd2525_png.jar;%OPENMAP_HOME%\lib\openmap.jar;%OPENMAP_HOME%\lib\omsvg.jar;%OPENMAP_HOME%\lib\omj3d.jar;%OPENMAP_HOME%\lib\omcorba_vb.jar;
 
 rem OK, now run OpenMap
-%JAVABIN% -Xmx256m -Dopenmap.configDir=%OPENMAP_HOME%\share -Ddebug.showprogress com.bbn.openmap.app.OpenMap
+%JAVABIN% -Xmx64m -Dopenmap.configDir=%OPENMAP_HOME%\share -Ddebug.showprogress com.bbn.openmap.app.OpenMap
