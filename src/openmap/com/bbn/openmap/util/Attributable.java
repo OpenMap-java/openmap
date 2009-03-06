258fe566963fbe30c1fde2de2aa57df19b263275
dietrick <dietrick@f68c23cc-7f87-4fbc-acfa-d9760b90acc1>
2009-03-06 17:02:42 +0000

    Adding an interface that says that the object has attributes.
    git-svn-id: svn+ssh://src.dsl.bbn.com/svn/openmap/trunk@1950 f68c23cc-7f87-4fbc-acfa-d9760b90acc1

---
+++ src/openmap/com/bbn/openmap/util/Attributable.java
@@ -0,0 +1,73 @@
+//**********************************************************************
+//
+//<copyright>
+//
+//BBN Technologies
+//10 Moulton Street
+//Cambridge, MA 02138
+//(617) 873-8000
+//
+//Copyright (C) BBNT Solutions LLC. All rights reserved.
+//
+//</copyright>
+//**********************************************************************
+//
+//$Source:
+///cvs/darwars/ambush/aar/src/com/bbn/ambush/mission/MissionHandler.java,v
+//$
+//$RCSfile: Attributable.java,v $
+//$Revision: 1.1 $
+//$Date: 2007/08/16 22:15:31 $
+//$Author: dietrick $
+//
+//**********************************************************************
+
+package com.bbn.openmap.util;
+
+import java.util.Map;
+
+/**
+ * An object that has attributes.
+ * 
+ * @author dietrick
+ */
+public interface Attributable {
+
+    /**
+     * Put an attribute in the object, the value can be retrieved later via the
+     * key.
+     * 
+     * @param key
+     * @param value
+     */
+    public void putAttribute(Object key, Object value);
+
+    /**
+     * Get the attribute stored under a key.
+     * 
+     * @param key
+     * @return value if found, null if not.
+     */
+    public Object getAttribute(Object key);
+
+    /**
+     * Tells the object to clear all of its attributes.
+     */
+    public void clearAttributes();
+
+    /**
+     * Convenience method to allow supperclass methods access to an attribute
+     * table if they are acting as a wrapper.
+     * 
+     * @return
+     */
+    public Map getAttributes();
+    
+    /**
+     * Convenience method to allow supperclass methods access to an attribute
+     * table if they are acting as a wrapper.
+     * 
+     * @return
+     */
+    public void setAttributes(Map map);
+}
