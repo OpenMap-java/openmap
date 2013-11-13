9f12b6371bc932257130f100ac0eead58ee9ed1b
Don Dietrick <dfdietrick@gmail.com>
2013-11-12 22:22:00 -0500

    Changes for Tag 5.0.2

--- src/openmap/com/bbn/openmap/MapBean.java
+++ src/openmap/com/bbn/openmap/MapBean.java
@@ -148,7 +148,7 @@ public class MapBean
    /**
     * OpenMap version.
     */
-   public static final String version = "5.0.2b";
+   public static final String version = "5.0.2";
 
    /**
     * Suppress the copyright message on initialization.
