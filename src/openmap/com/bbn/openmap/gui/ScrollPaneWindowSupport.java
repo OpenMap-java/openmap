99d519baee001d37cceac401a61de15fc2bb4a5d
Don Dietrick <dfdietrick@gmail.com>
2014-05-30 15:47:59 -0400

    Added Bart Jourquin's window support layout fix for Nimbus L&F

--- src/openmap/com/bbn/openmap/gui/ScrollPaneWindowSupport.java
+++ src/openmap/com/bbn/openmap/gui/ScrollPaneWindowSupport.java
@@ -1,34 +1,42 @@
-// **********************************************************************
-// 
-// <copyright>
-// 
-//  BBN Technologies
-//  10 Moulton Street
-//  Cambridge, MA 02138
-//  (617) 873-8000
-// 
-//  Copyright (C) BBNT Solutions LLC. All rights reserved.
-// 
-// </copyright>
-// **********************************************************************
-// 
-// $Source: /cvs/distapps/openmap/src/openmap/com/bbn/openmap/gui/ScrollPaneWindowSupport.java,v $
-// $RCSfile: ScrollPaneWindowSupport.java,v $
-// $Revision: 1.2 $
-// $Date: 2004/10/14 18:05:49 $
-// $Author: dietrick $
-// 
-// **********************************************************************
-
 package com.bbn.openmap.gui;
 
 import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
 
 import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneLayout;
 
 /**
- * The ScrollPaneWindowSupport class does the same thing as
- * WindowSupport, it just wraps content in a JScrollPane.
+ * Alternative ScrollPaneLayout that returns a preferred layout size that
+ * properly takes the scroll bars sizes into account. This is a workaround for a
+ * display problem with the Nimbus Look & Feel.
+ */
+class OMScrollPaneLayout extends ScrollPaneLayout {
+
+    @Override
+    public Dimension preferredLayoutSize(Container parent) {
+        Dimension dim = super.preferredLayoutSize(parent);
+        JScrollPane pane = (JScrollPane) parent;
+        Component comp = pane.getViewport().getView();
+        Dimension viewPref = comp.getPreferredSize();
+        Dimension port = pane.getViewport().getExtentSize();
+
+        if (port.height < viewPref.height) {
+            dim.width += pane.getVerticalScrollBar().getPreferredSize().width;
+        }
+
+        if (port.width < viewPref.width) {
+            dim.height += pane.getHorizontalScrollBar().getPreferredSize().height;
+        }
+        return dim;
+    }
+
+}
+
+/**
+ * The ScrollPaneWindowSupport class does the same thing as WindowSupport, it
+ * just wraps content in a JScrollPane.
  */
 public class ScrollPaneWindowSupport extends WindowSupport {
 
@@ -46,6 +54,15 @@ public class ScrollPaneWindowSupport extends WindowSupport {
      * Wrap content in a JScrollPane.
      */
     public Component modifyContent(Component comp) {
-        return new JScrollPane(comp);
+
+        JScrollPane pane = new JScrollPane(comp);
+
+        // Use an alternative layout in order to properly handle scroll bars
+        // with the Nimbus L&F
+        pane.setLayout(new OMScrollPaneLayout());
+
+        return pane;
+
     }
+
 }
