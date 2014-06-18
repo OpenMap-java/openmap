e59bbe02e23a8aaea5fcb43c0ca16270c5dc9f35
Don Dietrick <dfdietrick@gmail.com>
2014-06-18 14:05:45 -0400

    Updating trunk version number to 5.1.2b

--- src/openmap/com/bbn/openmap/MapBean.java
+++ src/openmap/com/bbn/openmap/MapBean.java
@@ -150,7 +150,7 @@ public class MapBean extends JComponent implements ComponentListener, ContainerL
     /**
      * OpenMap version.
      */
-    public static final String version = "5.1.1";
+    public static final String version = "5.1.2b";
 
     /**
      * Suppress the copyright message on initialization.
