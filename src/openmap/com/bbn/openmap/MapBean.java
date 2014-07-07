e385e3c9eea85e77fedca35e96713b08a4251428
Don Dietrick <dfdietrick@gmail.com>
2014-07-07 15:52:00 -0400

    Preparing for 5.1.2 tag.

--- src/openmap/com/bbn/openmap/MapBean.java
+++ src/openmap/com/bbn/openmap/MapBean.java
@@ -150,7 +150,7 @@ public class MapBean extends JComponent implements ComponentListener, ContainerL
     /**
      * OpenMap version.
      */
-    public static final String version = "5.1.2b";
+    public static final String version = "5.1.2";
 
     /**
      * Suppress the copyright message on initialization.
