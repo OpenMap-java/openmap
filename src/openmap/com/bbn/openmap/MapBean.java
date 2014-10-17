29dc0750911fb24f1a55623b34c93ccfe645b97e
Don Dietrick <dfdietrick@gmail.com>
2014-10-17 00:58:47 -0400

    Updating version numbers for 5.1.7b

--- src/openmap/com/bbn/openmap/MapBean.java
+++ src/openmap/com/bbn/openmap/MapBean.java
@@ -150,7 +150,7 @@ public class MapBean extends JComponent implements ComponentListener, ContainerL
     /**
      * OpenMap version.
      */
-    public static final String version = "5.1.6";
+    public static final String version = "5.1.7b";
 
     /**
      * Suppress the copyright message on initialization.
