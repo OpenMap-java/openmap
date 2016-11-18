5846d3731b0e9816ae4275e77e07b67714527a2d
Don Dietrick <support@openmap-java.org>
2016-11-18 13:24:28 -0500

    Added Generic Type to PropUtils.parseColorFromProperties so the return type matches the default object type sent in.
    The other classes in this change have been updated to not cast the returned value.

--- src/core/src/main/java/com/bbn/openmap/gui/event/EventListCellRenderer.java
+++ src/core/src/main/java/com/bbn/openmap/gui/event/EventListCellRenderer.java
@@ -42,153 +42,143 @@ import com.bbn.openmap.omGraphics.DrawingAttributes;
 import com.bbn.openmap.util.ComponentFactory;
 import com.bbn.openmap.util.PropUtils;
 
-public class EventListCellRenderer extends OMComponentPanel implements
-        ListCellRenderer {
+public class EventListCellRenderer extends OMComponentPanel implements ListCellRenderer {
 
-    protected JLabel label = new JLabel();
-    protected JLabel timeMark = new JLabel();
-    protected JLabel ratingMark = new JLabel();
+	protected JLabel label = new JLabel();
+	protected JLabel timeMark = new JLabel();
+	protected JLabel ratingMark = new JLabel();
 
-    public Color fontColor = Color.BLACK;
-    public Color altFontColor = Color.BLACK;
-    public Color selectColor = Color.GRAY;
-    public Color timeWindowColor = Color.LIGHT_GRAY;
-    public Color regularBackgroundColor = Color.WHITE;
+	public Color fontColor = Color.BLACK;
+	public Color altFontColor = Color.BLACK;
+	public Color selectColor = Color.GRAY;
+	public Color timeWindowColor = Color.LIGHT_GRAY;
+	public Color regularBackgroundColor = Color.WHITE;
 
-    public static final String FontColorProperty = "fontColor";
-    public static final String AltFontColorProperty = "altFontColor";
-    public static final String SelectColorProperty = "selectColor";
-    public static final String TimeWindowColorProperty = "timeWindowColor";
-    public static final String BackgroundColorProperty = "color";
-    public static final String IconPackageClassProperty = "iconPackageClass";
+	public static final String FontColorProperty = "fontColor";
+	public static final String AltFontColorProperty = "altFontColor";
+	public static final String SelectColorProperty = "selectColor";
+	public static final String TimeWindowColorProperty = "timeWindowColor";
+	public static final String BackgroundColorProperty = "color";
+	public static final String IconPackageClassProperty = "iconPackageClass";
 
-    /**
-     * This cell renderer is pretty closely tied to the icon package. If you
-     * want to change one, you'll probably want to change the other, too.
-     */
-    protected EventListIconPackage iconPackage;
+	/**
+	 * This cell renderer is pretty closely tied to the icon package. If you
+	 * want to change one, you'll probably want to change the other, too.
+	 */
+	protected EventListIconPackage iconPackage;
 
-    public EventListCellRenderer() {
-        init();
-    }
+	public EventListCellRenderer() {
+		init();
+	}
 
-    protected void init() {
-        removeAll();
-        setOpaque(true);
-        GridBagLayout gridbag = new GridBagLayout();
-        GridBagConstraints c = new GridBagConstraints();
-        setLayout(gridbag);
+	protected void init() {
+		removeAll();
+		setOpaque(true);
+		GridBagLayout gridbag = new GridBagLayout();
+		GridBagConstraints c = new GridBagConstraints();
+		setLayout(gridbag);
 
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 1.0f;
-        gridbag.setConstraints(label, c);
-        this.add(label);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 1.0f;
+		gridbag.setConstraints(label, c);
+		this.add(label);
 
-        c.fill = GridBagConstraints.NONE;
-        c.weightx = 0f;
-        gridbag.setConstraints(ratingMark, c);
-        this.add(ratingMark);
-        gridbag.setConstraints(timeMark, c);
-        this.add(timeMark);
+		c.fill = GridBagConstraints.NONE;
+		c.weightx = 0f;
+		gridbag.setConstraints(ratingMark, c);
+		this.add(ratingMark);
+		gridbag.setConstraints(timeMark, c);
+		this.add(timeMark);
 
-        Font f = label.getFont();
-        f = new Font(f.getName(), f.getStyle(), f.getSize() - 1);
-        label.setFont(f);
+		Font f = label.getFont();
+		f = new Font(f.getName(), f.getStyle(), f.getSize() - 1);
+		label.setFont(f);
 
-        // This gets icon package set up
-        setPreferredSize(new Dimension(20, getIconPackage().getButtonSize()));
-    }
+		// This gets icon package set up
+		setPreferredSize(new Dimension(20, getIconPackage().getButtonSize()));
+	}
 
-    public EventListIconPackage getIconPackage() {
-        if (iconPackage == null) {
-            iconPackage = new EventListIconPackage();
-        }
-        return iconPackage;
-    }
+	public EventListIconPackage getIconPackage() {
+		if (iconPackage == null) {
+			iconPackage = new EventListIconPackage();
+		}
+		return iconPackage;
+	}
 
-    public void setIconPackage(EventListIconPackage iconPackage) {
-        this.iconPackage = iconPackage;
-    }
+	public void setIconPackage(EventListIconPackage iconPackage) {
+		this.iconPackage = iconPackage;
+	}
 
-    public void setProperties(String prefix, Properties props) {
-        super.setProperties(prefix, props);
-        prefix = PropUtils.getScopedPropertyPrefix(prefix);
+	public void setProperties(String prefix, Properties props) {
+		super.setProperties(prefix, props);
+		prefix = PropUtils.getScopedPropertyPrefix(prefix);
 
-        fontColor = (Color) PropUtils.parseColorFromProperties(props, prefix
-                + FontColorProperty, fontColor);
-        altFontColor = (Color) PropUtils.parseColorFromProperties(props, prefix
-                + AltFontColorProperty, altFontColor);
-        selectColor = (Color) PropUtils.parseColorFromProperties(props, prefix
-                + SelectColorProperty, selectColor);
-        timeWindowColor = (Color) PropUtils.parseColorFromProperties(props,
-                prefix + TimeWindowColorProperty,
-                timeWindowColor);
-        regularBackgroundColor = (Color) PropUtils.parseColorFromProperties(props,
-                prefix + BackgroundColorProperty,
-                regularBackgroundColor);
-        
-        String crc = props.getProperty(prefix + IconPackageClassProperty);
-        if (crc != null) {
-            iconPackage = (EventListIconPackage) ComponentFactory.create(crc, prefix, props);
-            init();
-        }
+		fontColor = PropUtils.parseColorFromProperties(props, prefix + FontColorProperty, fontColor);
+		altFontColor = PropUtils.parseColorFromProperties(props, prefix + AltFontColorProperty, altFontColor);
+		selectColor = PropUtils.parseColorFromProperties(props, prefix + SelectColorProperty, selectColor);
+		timeWindowColor = PropUtils.parseColorFromProperties(props, prefix + TimeWindowColorProperty, timeWindowColor);
+		regularBackgroundColor = PropUtils.parseColorFromProperties(props, prefix + BackgroundColorProperty,
+				regularBackgroundColor);
 
-    }
+		String crc = props.getProperty(prefix + IconPackageClassProperty);
+		if (crc != null) {
+			iconPackage = (EventListIconPackage) ComponentFactory.create(crc, prefix, props);
+			init();
+		}
 
-    public DrawingAttributes setRenderingAttributes(
-                                                    DrawingAttributes drawingAttributes) {
+	}
 
-        if (drawingAttributes == null) {
-            drawingAttributes = DrawingAttributes.getDefaultClone();
-        }
+	public DrawingAttributes setRenderingAttributes(DrawingAttributes drawingAttributes) {
 
-        drawingAttributes.setFillPaint(regularBackgroundColor);
-        drawingAttributes.setSelectPaint(selectColor);
-        drawingAttributes.setLinePaint(fontColor);
-        drawingAttributes.setMattingPaint(timeWindowColor);
-        return drawingAttributes;
-    }
+		if (drawingAttributes == null) {
+			drawingAttributes = DrawingAttributes.getDefaultClone();
+		}
 
-    public Component getListCellRendererComponent(JList list, Object value,
-                                                  int index,
-                                                  boolean isSelected,
-                                                  boolean cellHasFocus) {
-        label.setText(value.toString());
+		drawingAttributes.setFillPaint(regularBackgroundColor);
+		drawingAttributes.setSelectPaint(selectColor);
+		drawingAttributes.setLinePaint(fontColor);
+		drawingAttributes.setMattingPaint(timeWindowColor);
+		return drawingAttributes;
+	}
 
-        if (value instanceof OMEvent) {
-            OMEvent OMe = (OMEvent) value;
-            isSelected = OMe.getAttribute(OMEvent.ATT_KEY_SELECTED) != null;
+	public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected,
+			boolean cellHasFocus) {
+		label.setText(value.toString());
 
-            timeMark.setIcon(OMe.getAttribute(OMEvent.ATT_KEY_PLAY_FILTER) == Boolean.TRUE ? iconPackage.clockImage
-                    : iconPackage.invisibleImage);
+		if (value instanceof OMEvent) {
+			OMEvent OMe = (OMEvent) value;
+			isSelected = OMe.getAttribute(OMEvent.ATT_KEY_SELECTED) != null;
 
-            Object rating = OMe.getAttribute(OMEvent.ATT_KEY_RATING);
-            if (rating == OMEvent.ATT_VAL_BAD_RATING) {
-                ratingMark.setIcon(iconPackage.thumbsDownImage);
-            } else if (rating == OMEvent.ATT_VAL_GOOD_RATING) {
-                ratingMark.setIcon(iconPackage.thumbsUpImage);
-            } else {
-                ratingMark.setIcon(iconPackage.invisibleImage);
-            }
+			timeMark.setIcon(OMe.getAttribute(OMEvent.ATT_KEY_PLAY_FILTER) == Boolean.TRUE ? iconPackage.clockImage
+					: iconPackage.invisibleImage);
 
-            if (OMe.isAtCurrentTime()) {
-                if (isSelected) {
-                    setBackground(selectColor);
-                } else {
-                    setBackground(timeWindowColor);
-                }
-                setForeground(altFontColor);
-                return this;
-            }
+			Object rating = OMe.getAttribute(OMEvent.ATT_KEY_RATING);
+			if (rating == OMEvent.ATT_VAL_BAD_RATING) {
+				ratingMark.setIcon(iconPackage.thumbsDownImage);
+			} else if (rating == OMEvent.ATT_VAL_GOOD_RATING) {
+				ratingMark.setIcon(iconPackage.thumbsUpImage);
+			} else {
+				ratingMark.setIcon(iconPackage.invisibleImage);
+			}
 
-        } else {
-            timeMark.setIcon(iconPackage.invisibleImage);
-            ratingMark.setIcon(iconPackage.invisibleImage);
-        }
+			if (OMe.isAtCurrentTime()) {
+				if (isSelected) {
+					setBackground(selectColor);
+				} else {
+					setBackground(timeWindowColor);
+				}
+				setForeground(altFontColor);
+				return this;
+			}
 
-        setBackground(isSelected ? selectColor : regularBackgroundColor);
-        setForeground(isSelected ? altFontColor : fontColor);
-        return this;
-    }
+		} else {
+			timeMark.setIcon(iconPackage.invisibleImage);
+			ratingMark.setIcon(iconPackage.invisibleImage);
+		}
+
+		setBackground(isSelected ? selectColor : regularBackgroundColor);
+		setForeground(isSelected ? altFontColor : fontColor);
+		return this;
+	}
 
 }
