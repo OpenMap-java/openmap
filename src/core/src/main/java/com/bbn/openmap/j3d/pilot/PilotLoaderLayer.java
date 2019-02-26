0ab68e383b51d8308915136af9ae0712876301a9
Don Dietrick <dfdietrick@gmail.com>
2019-02-26 15:38:55 -0500

    Changed setPilotLoader to setGraphicLoader to match convention.

--- src/core/src/main/java/com/bbn/openmap/j3d/pilot/PilotLoaderLayer.java
+++ src/core/src/main/java/com/bbn/openmap/j3d/pilot/PilotLoaderLayer.java
@@ -41,127 +41,122 @@ import com.bbn.openmap.util.Debug;
  */
 public class PilotLoaderLayer extends GraphicLoaderLayer {
 
-    protected PilotLoader loader = null;
-    protected OMDrawingTool dt = null;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
-    public PilotLoaderLayer() {
-        super();
-        setPilotLoader(new PilotLoader(this));
-    }
+	protected OMDrawingTool dt = null;
 
-    /**
-     * The getRectangle call is the main call into the PlugIn module. The module
-     * is expected to fill the graphics list with objects that are within the
-     * screen parameters passed.
-     * 
-     * @return OMGraphic that should be displayed for the current projection.
-     */
-    public OMGraphicList prepare() {
-        Projection p = getProjection();
+	public PilotLoaderLayer() {
+		super();
+		setGraphicLoader(new PilotLoader(this));
+	}
 
-        if (loader != null) {
-            loader.setProjection(p);
-        }
+	/**
+	 * The getRectangle call is the main call into the PlugIn module. The module
+	 * is expected to fill the graphics list with objects that are within the
+	 * screen parameters passed.
+	 * 
+	 * @return OMGraphic that should be displayed for the current projection.
+	 */
+	public OMGraphicList prepare() {
+		Projection p = getProjection();
 
-        OMGraphicList list = new OMGraphicList(getList());
-        list.generate(p);
+		if (loader != null) {
+			loader.setProjection(p);
+		}
 
-        if (Debug.debugging("pilotloader")) {
-            Debug.output("GraphicLoaderPlugIn returning list of " + list.size()
-                    + " objects.");
-        }
+		OMGraphicList list = new OMGraphicList(getList());
+		list.generate(p);
 
-        return list;
-    }
+		if (Debug.debugging("pilotloader")) {
+			Debug.output("GraphicLoaderPlugIn returning list of " + list.size() + " objects.");
+		}
 
-    public synchronized void setList(OMGraphicList graphics) {
-        super.setList(graphics);
-        doPrepare();
-    }
+		return list;
+	}
 
-    public synchronized boolean doAction(OMGraphic graphic, OMAction action) {
-        boolean ret = super.doAction(graphic, action);
-        doPrepare();
-        return ret;
-    }
+	public synchronized void setList(OMGraphicList graphics) {
+		super.setList(graphics);
+		doPrepare();
+	}
 
-    /**
-     * Get the path/point loader.
-     */
-    public void setPilotLoader(PilotLoader pl) {
-        loader = pl;
-        setMapMouseListener(loader);
+	public synchronized boolean doAction(OMGraphic graphic, OMAction action) {
+		boolean ret = super.doAction(graphic, action);
+		doPrepare();
+		return ret;
+	}
 
-        try {
-            loader.setBeanContext(getBeanContext());
-        } catch (PropertyVetoException pve) {
-        }
-    }
+	/**
+	 * Get the path/point loader.
+	 */
+	public void setGraphicLoader(PilotLoader pl) {
+		loader = pl;
+		setMapMouseListener(pl);
 
-    /**
-     * Get the path/point loader.
-     */
-    public PilotLoader getPilotLoader() {
-        return loader;
-    }
+		try {
+			pl.setBeanContext(getBeanContext());
+		} catch (PropertyVetoException pve) {
+		}
+	}
 
-    public Component getGUI() {
-        if (loader != null) {
-            return loader.getGUI();
-        } else {
-            return null;
-        }
-    }
+	public Component getGUI() {
+		if (loader != null) {
+			return loader.getGUI();
+		} else {
+			return null;
+		}
+	}
 
-    /**
-     * Invoked when the mouse has been clicked on a component. The listener will
-     * receive this event if it successfully processed
-     * <code>mousePressed()</code>, or if no other listener processes the event.
-     * If the listener successfully processes <code>mouseClicked()</code>, then
-     * it will receive the next <code>mouseClicked()</code> notifications that
-     * have a click count greater than one.
-     * <p>
-     * NOTE: We have noticed that this method can sometimes be erroneously
-     * invoked. It seems to occur when a light-weight AWT component (like an
-     * internal window or menu) closes (removes itself from the window
-     * hierarchy). A specific OpenMap example is when you make a menu selection
-     * when the MenuItem you select is above the MapBean canvas. After making
-     * the selection, the mouseClicked() gets invoked on the MouseDelegator,
-     * which passes it to the appropriate listeners depending on the MouseMode.
-     * The best way to avoid this problem is to not implement anything crucial
-     * in this method. Use a combination of <code>mousePressed()</code> and
-     * <code>mouseReleased()</code> instead.
-     * 
-     * @param e MouseEvent
-     * @return true if the listener was able to process the event.
-     */
-    public boolean mouseClicked(MouseEvent e) {
+	/**
+	 * Invoked when the mouse has been clicked on a component. The listener will
+	 * receive this event if it successfully processed
+	 * <code>mousePressed()</code>, or if no other listener processes the event.
+	 * If the listener successfully processes <code>mouseClicked()</code>, then
+	 * it will receive the next <code>mouseClicked()</code> notifications that
+	 * have a click count greater than one.
+	 * <p>
+	 * NOTE: We have noticed that this method can sometimes be erroneously
+	 * invoked. It seems to occur when a light-weight AWT component (like an
+	 * internal window or menu) closes (removes itself from the window
+	 * hierarchy). A specific OpenMap example is when you make a menu selection
+	 * when the MenuItem you select is above the MapBean canvas. After making
+	 * the selection, the mouseClicked() gets invoked on the MouseDelegator,
+	 * which passes it to the appropriate listeners depending on the MouseMode.
+	 * The best way to avoid this problem is to not implement anything crucial
+	 * in this method. Use a combination of <code>mousePressed()</code> and
+	 * <code>mouseReleased()</code> instead.
+	 * 
+	 * @param e MouseEvent
+	 * @return true if the listener was able to process the event.
+	 */
+	public boolean mouseClicked(MouseEvent e) {
 
-        OMGraphicList list = getList();
-        
-        if (list != null) {
-            OMGraphic graphic = list.getContains(e.getX(), e.getY());
-            if (graphic instanceof Pilot) {
-                Pilot mp = (Pilot) graphic;
-                mp.showPalette();
-                return true;
-            }
-        }
-        return false;
-    }
+		OMGraphicList list = getList();
 
-    /**
-     * Method for BeanContextChild interface. Adds this object as a
-     * BeanContextMembership listener, set the BeanContext in this objects
-     * BeanContextSupport, and receives the initial list of objects currently
-     * contained in the BeanContext.
-     */
-    public void setBeanContext(BeanContext in_bc) throws PropertyVetoException {
-        super.setBeanContext(in_bc);
-        if (loader != null) {
-            loader.setBeanContext(in_bc);
-        }
-    }
+		if (list != null) {
+			OMGraphic graphic = list.getContains(e.getX(), e.getY());
+			if (graphic instanceof Pilot) {
+				Pilot mp = (Pilot) graphic;
+				mp.showPalette();
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Method for BeanContextChild interface. Adds this object as a
+	 * BeanContextMembership listener, set the BeanContext in this objects
+	 * BeanContextSupport, and receives the initial list of objects currently
+	 * contained in the BeanContext.
+	 */
+	public void setBeanContext(BeanContext in_bc) throws PropertyVetoException {
+		super.setBeanContext(in_bc);
+		if (loader instanceof PilotLoader) {
+			((PilotLoader) loader).setBeanContext(in_bc);
+		}
+	}
 
 } // end PilotLoaderLayer.
-
